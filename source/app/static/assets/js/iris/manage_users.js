48558161e1919b938955eee3c1aeb61b5450e555
whikernel <whitekernel@whitekernel.fr>
2022-01-12 07:49:36 +0100

    [ADD] Visual insight of activness of user `

--- source/app/static/assets/js/iris/manage_users.js
+++ source/app/static/assets/js/iris/manage_users.js
@@ -92,7 +92,14 @@ $('#users_table').dataTable( {
           },
             { "data": "user_active",
             "render": function (data, type, row, meta) {
-                if (type === 'display') { data = sanitizeHTML(data);}
+                if (type === 'display') {
+                    data = sanitizeHTML(data);
+                    if (data == true) {
+                        data = '<span class="badge ml-2 badge-success">Active</span>';
+                    } else {
+                        data = '<span class="badge ml-2 badge-warning">Disabled</span>';
+                    }
+                }
                 return data;
               }
             }
