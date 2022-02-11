fb2b2a3f1657af10585a778cb7ba7f29b42bbcfb
whikernel <whitekernel@whitekernel.fr>
2022-02-11 20:13:26 +0100

    [#64][ADD] Alembic upgrade for attributes

---
+++ source/app/alembic/versions/2df770a4989c_add_objects_attributes.py
@@ -0,0 +1,24 @@
+"""Add objects attributes
+
+Revision ID: 2df770a4989c
+Revises: 10a7616f3cc7
+Create Date: 2022-02-11 20:13:14.365469
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2df770a4989c'
+down_revision = '10a7616f3cc7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
