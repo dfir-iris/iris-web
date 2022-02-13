c7d1a9810bc4ef287507d569165eed6a0db32571
whikernel <whitekernel@whitekernel.fr>
2022-02-13 11:40:56 +0100

    [#64][ADD] Focus mode for notes

--- source/app/static/assets/js/iris/common.js
+++ source/app/static/assets/js/iris/common.js
@@ -659,6 +659,17 @@ function update_time() {
     $('#current_date').text((new Date()).toLocaleString().slice(0, 17));
 }
 
+function toggle_focus_mode() {
+    class_a = "bg-focus-gradient"
+    $(".modal-case-focus").each(function (i, el)  {
+        if ($(el).hasClass( class_a )) {
+            $(el).removeClass(class_a, 1000);
+        } else {
+            $(el).addClass(class_a, 1000);
+        }
+    });
+}
+
 $(document).ready(function(){
     update_time();
     setInterval(function() { update_time(); }, 30000);
