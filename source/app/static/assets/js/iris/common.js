58178ec4bd360fbf6fc88a7840ee4d89c44936e4
whikernel <whitekernel@whitekernel.fr>
2022-04-19 18:36:50 +0200

    [ADD] Additional check if file is selected before evidence hashing

--- source/app/static/assets/js/iris/common.js
+++ source/app/static/assets/js/iris/common.js
@@ -366,13 +366,17 @@ function hide_loader() {
 }
 
 function get_hash() {
-  getMD5(
-    document.getElementById("input_autofill").files[0],
+    if (document.getElementById("input_autofill").files[0] === undefined) {
+        $('#btn_rfile_proc').text("Please select a file");
+        return;
+    }
+    getMD5(
+        document.getElementById("input_autofill").files[0],
     prog => $('#btn_rfile_proc').text("Processing "+ (prog * 100).toFixed(2) + "%")
-  ).then(
-    res => on_done_hash(res),
-    err => console.error(err)
-  );
+    ).then(
+        res => on_done_hash(res),
+        err => console.error(err)
+    );
 }
 
 
