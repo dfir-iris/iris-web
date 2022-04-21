fbb4e1184c40bc551580e56e4d416e5b5217d11c
sebastiandemmer <sebastian.demmer@gmx.net>
2022-04-21 22:16:48 +0100

    alembic db migration added

---
+++ source/app/alembic/versions/ff917e2ab02e_changed_the_asset_type_table_for_custom_.py
@@ -0,0 +1,24 @@
+"""changed the asset_type table for custom icons
+
+Revision ID: ff917e2ab02e
+Revises: b664ca1203a4
+Create Date: 2022-04-21 22:14:55.815983
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ff917e2ab02e'
+down_revision = 'b664ca1203a4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
