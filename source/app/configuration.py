bc51316e195c357251ea18fcdd9c15258fc5ddd2
whikernel <whitekernel@whitekernel.fr>
2022-04-27 19:11:08 +0200

    [UPD] Changed for long key IDs

--- source/app/configuration.py
+++ source/app/configuration.py
@@ -87,7 +87,7 @@ class Config():
 
     #RELEASE_URL = 'https://api.github.com/repos/dfir-iris/iris-web/releases'
     RELEASE_URL = 'http://192.168.1.11:8088/releases'
-    RELEASE_SIGNATURE_KEY = "3003B1BA1A2E235E"
+    RELEASE_SIGNATURE_KEY = "B9B762555CE8751E57B178DD3003B1BA1A2E235E"
 
     if os.environ.get('IRIS_WORKER') is None:
         CSRF_ENABLED = True
