a194e9e879f60937f5c7f7660dc533d384153263
whikernel <whitekernel@whitekernel.fr>
2022-04-29 07:53:49 +0200

    [IMP] UI for updates

--- source/app/static/assets/js/iris/update_handler.js
+++ source/app/static/assets/js/iris/update_handler.js
@@ -14,6 +14,9 @@ function add_update_log(message, is_error) {
         html_wrap = `<h4><i class="mt-2 fas fa-times text-danger"></i> `
     }
     $("#updates_log").append(html_wrap + message + '</h4><br/>')
+    $('#container-updates').animate({
+        scrollTop: $("#updates_log_end").offset().top
+    }, 50);
 }
 
 function get_caseid() {
