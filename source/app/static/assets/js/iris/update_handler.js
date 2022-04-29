894d4765622d393ad9b7e75d1b513cb2185287ee
whikernel <whitekernel@whitekernel.fr>
2022-04-29 19:10:18 +0200

    [ADD] Pre indications before updates

--- source/app/static/assets/js/iris/update_handler.js
+++ source/app/static/assets/js/iris/update_handler.js
@@ -118,8 +118,15 @@ function ping_check_server_online() {
 
 function start_updates(){
     $('#update_start_btn').hide();
+    $('.update_start_txt').hide();
+    $('#container-updates').show();
     update_socket.emit('update_get_current_version', { 'channel': channel });
     update_socket.emit('update_ping', { 'channel': channel });
+//    index = 0;
+//    while(index < 20) {
+//        add_update_log('ping');
+//        index += 1;
+//    }
 }
 
 
