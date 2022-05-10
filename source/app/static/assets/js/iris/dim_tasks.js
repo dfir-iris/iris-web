1ade3cbabd0bb353c28d9979229ae51cbd634b68
whikernel <whitekernel@whitekernel.fr>
2022-05-10 08:01:02 +0200

    [FIX] Error reporting in get activities

--- source/app/static/assets/js/iris/dim_tasks.js
+++ source/app/static/assets/js/iris/dim_tasks.js
@@ -67,16 +67,14 @@ $("#activities_table").css("font-size", 12);
 function get_activities () {
 
     get_request_api('/dim/tasks/list/1000')
-    .done( function(data) {
-         if (data.status == "success") {
+    .done((data) => {
+         if (notify_auto_api(data, true)) {
             content = data.data;
             Table.clear();
             Table.rows.add(content);
             Table.columns.adjust().draw();
             $('#feed_last_updated').text("Last updated: " + new Date().toLocaleTimeString());
             hide_loader();
-        } else {
-            $('#modal_customer_message').text(data.message);
         }
     });
 
