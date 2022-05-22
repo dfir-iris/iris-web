2c5e58f87c4728ef3d7106465ff0673228fea108
whikernel <whitekernel@whitekernel.fr>
2022-05-22 09:25:58 +0200

    [ADD] Passwd info in file info modal

--- source/app/schema/marshables.py
+++ source/app/schema/marshables.py
@@ -295,16 +295,13 @@ class DSFileSchema(ma.SQLAlchemyAutoSchema):
                 try:
 
                     temp_location = Path('/tmp/') / location.name
-                    print(f'temp_location : {temp_location}')
                     file_storage.save(temp_location)
                     file_hash = file_sha256sum(temp_location)
                     file_size = temp_location.stat().st_size
 
                     temp_location = temp_location.rename(Path('/tmp/') / file_hash)
-                    print(f'New temp_location : {temp_location}')
 
                     pyminizip.compress(temp_location.as_posix(), None, location.as_posix() + '.zip', passwd, 0)
-                    print(f'Zip location : {location.as_posix()}')
 
                     Path(temp_location).unlink()
 
