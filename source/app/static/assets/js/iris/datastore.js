16decac9ff954828a692f059fc9899f0c71ecb07
whikernel <whitekernel@whitekernel.fr>
2022-05-23 08:16:50 +0200

    [ADD] Auto fill of filename when adding a file

--- source/app/static/assets/js/iris/datastore.js
+++ source/app/static/assets/js/iris/datastore.js
@@ -1,28 +1,31 @@
-var ds_filter = ace.edit("ds_file_search",
-{
-    autoScrollEditorIntoView: true,
-    minLines: 1,
-    maxLines: 5
-});
-ds_filter.setTheme("ace/theme/tomorrow");
-ds_filter.session.setMode("ace/mode/json");
-ds_filter.renderer.setShowGutter(false);
-ds_filter.setShowPrintMargin(false);
-ds_filter.renderer.setScrollMargin(10, 10);
-ds_filter.setOption("displayIndentGuides", true);
-ds_filter.setOption("indentedSoftWrap", true);
-ds_filter.setOption("showLineNumbers", false);
-ds_filter.setOption("placeholder", "Search files");
-ds_filter.setOption("highlightActiveLine", false);
-ds_filter.commands.addCommand({
-        name: "Do filter",
-        bindKey: { win: "Enter", mac: "Enter" },
-        exec: function (editor) {
-                  filter_files();
-        }
-});
+var ds_filter;
 
 function load_datastore() {
+
+    ds_filter = ace.edit("ds_file_search",
+    {
+        autoScrollEditorIntoView: true,
+        minLines: 1,
+        maxLines: 5
+    });
+    ds_filter.setTheme("ace/theme/tomorrow");
+    ds_filter.session.setMode("ace/mode/json");
+    ds_filter.renderer.setShowGutter(false);
+    ds_filter.setShowPrintMargin(false);
+    ds_filter.renderer.setScrollMargin(10, 10);
+    ds_filter.setOption("displayIndentGuides", true);
+    ds_filter.setOption("indentedSoftWrap", true);
+    ds_filter.setOption("showLineNumbers", false);
+    ds_filter.setOption("placeholder", "Search files");
+    ds_filter.setOption("highlightActiveLine", false);
+    ds_filter.commands.addCommand({
+            name: "Do filter",
+            bindKey: { win: "Enter", mac: "Enter" },
+            exec: function (editor) {
+                      filter_files();
+            }
+    });
+
     get_request_api('/datastore/list/tree')
     .done(function (data){
         if(notify_auto_api(data, true)){
