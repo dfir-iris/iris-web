3743e40e125222bd600bdbfa12939b5ef16e6c6a
whikernel <whitekernel@whitekernel.fr>
2022-05-25 18:09:32 +0200

    [#116][ADD] Alembic migration script

---
+++ source/app/alembic/versions/cd519d2d24df_password_policy_edition.py
@@ -0,0 +1,24 @@
+"""Password policy edition
+
+Revision ID: cd519d2d24df
+Revises: ca93d4b54571
+Create Date: 2022-05-25 18:09:08.741619
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cd519d2d24df'
+down_revision = 'ca93d4b54571'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
