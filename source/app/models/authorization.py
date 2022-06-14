dc05ea46c44aecacc325fcde0fa845cecb537eb9
whikernel <whitekernel@whitekernel.fr>
2022-06-14 08:00:33 +0200

    [#49][ADD] Organisation model

--- source/app/models/authorization.py
+++ source/app/models/authorization.py
@@ -1,42 +1,67 @@
+import uuid
+
 import secrets
 
 from flask_login import UserMixin
+from sqlalchemy import BigInteger
+from sqlalchemy import Boolean
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Text
 from sqlalchemy import or_
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 from app import db
 
 
+class Organisation(db.Model):
+    __tablename__ = 'organisations'
+
+    org_id = Column(BigInteger, primary_key=True)
+    org_uuid = Column(UUID(as_uuid=True), default=uuid.uuid4)
+    org_name = Column(Text, nullable=False, unique=True)
+    org_description = Column(Text)
+    org_url = Column(Text)
+    org_logo = Column(Text)
+    org_email = Column(Text)
+    org_nationality = Column(Text)
+    org_sector = Column(Text)
+    org_type = Column(Text)
+
+
 class Role(db.Model):
     __tablename__ = 'roles'
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(50), unique=True)
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(50), unique=True)
 
 
 # Define the UserRoles association table
 class UserRoles(db.Model):
     __tablename__ = 'user_roles'
-    id = db.Column(db.Integer(), primary_key=True)
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
-    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+    id = Column(Integer(), primary_key=True)
+    user_id = Column(Integer(), ForeignKey('user.id', ondelete='CASCADE'))
+    role_id = Column(Integer(), ForeignKey('roles.id', ondelete='CASCADE'))
 
 
 class User(UserMixin, db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(64), unique=True)
-    name = db.Column(db.String(64), unique=False)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(500))
-    ctx_case = db.Column(Integer)
-    ctx_human_case = db.Column(db.String(256))
-    active = db.Column(db.Boolean())
-    api_key = db.Column(db.Text(), unique=True)
-    external_id = db.Column(db.Text, unique=True)
-    in_dark_mode = db.Column(db.Boolean())
+    id = Column(db.Integer, primary_key=True)
+    user = Column(String(64), unique=True)
+    name = Column(String(64), unique=False)
+    email = Column(String(120), unique=True)
+    password = Column(String(500))
+    ctx_case = Column(Integer)
+    ctx_human_case = Column(String(256))
+    active = Column(Boolean())
+    api_key = Column(Text(), unique=True)
+    external_id = Column(Text, unique=True)
+    in_dark_mode = Column(Boolean())
 
-    roles = db.relationship('Role', secondary='user_roles')
+    roles = relationship('Role', secondary='user_roles')
 
     def __init__(self, user: str, name: str, email: str, password: str, active: str, external_id: str = None):
         self.user = user
@@ -78,3 +103,4 @@ class User(UserMixin, db.Model):
         if "administrator" in roles:
             return True
         return False
+
