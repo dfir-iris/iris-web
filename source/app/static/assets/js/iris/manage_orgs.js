ca2cc558d08ca03251235d3ebc3307030a234920
whikernel <whitekernel@whitekernel.fr>
2022-06-17 08:19:06 +0200

    [#49][ADD] Org listing on UI

---
+++ source/app/static/assets/js/iris/manage_orgs.js
@@ -0,0 +1,217 @@
+$('#org_table').dataTable( {
+    "ajax": {
+      "url": "organisations/list" + case_param(),
+      "contentType": "application/json",
+      "type": "GET",
+      "data": function ( d ) {
+        if (d.status == 'success') {
+          return JSON.stringify( d.data );
+        } else {
+          return [];
+        }
+      }
+    },
+    "order": [[ 1, "asc" ]],
+    "autoWidth": false,
+    "columns": [
+        {
+            "data": "org_id",
+            "render": function ( data, type, row ) {
+                if (type === 'display') {
+                    data = sanitizeHTML(data)
+                    return '<a href="#" onclick="org_detail(\'' + row["org_id"] + '\');">' + data +'</a>';
+                }
+                return data;
+            }
+        },
+        { "data": "org_name",
+          "render": function ( data, type, row ) {
+                if (type === 'display') {
+                    data = sanitizeHTML(data)
+                    return '<a href="#" onclick="group_detail(\'' + row["org_id"] + '\');">' + data +'</a>';
+                }
+                return data;
+            }
+        },
+        { "data": "org_description",
+          "render": function (data, type, row, meta) {
+            if (type === 'display') { data = sanitizeHTML(data);}
+            return data;
+          }
+        },
+        { "data": "org_nationality",
+          "render": function (data, type, row, meta) {
+                if (type === 'display') {
+                    data = sanitizeHTML(data);
+                }
+                return data;
+              }
+        },
+        { "data": "org_sector",
+            "render": function ( data, type, row ) {
+                if (type === 'display') {
+                    data = sanitizeHTML(data);
+                }
+                return data;
+            }
+        }
+      ]
+    }
+);
+
+function refresh_groups(do_notify) {
+  $('#groups_table').DataTable().ajax.reload();
+  if (do_notify !== undefined) {
+    notify_success("Refreshed");
+  }
+}
+
+
+/* Fetch the details of an user and allow modification */
+function group_detail(group_id) {
+    url = 'groups/' + group_id + '/modal' + case_param();
+    $('#modal_access_control').load(url, function (response, status, xhr) {
+        if (status !== "success") {
+             ajax_notify_error(xhr, url);
+             return false;
+        }
+
+        $('#submit_new_group').on("click", function () {
+            clear_api_error();
+
+            var data_sent = $('#form_new_group').serializeObject();
+            post_request_api('/manage/groups/update/' + group_id, JSON.stringify(data_sent), true)
+            .done((data) => {
+                if(notify_auto_api(data)) {
+                    refresh_users();
+                    $('#modal_access_control').modal('hide');
+                }
+            });
+
+            return false;
+        })
+
+
+    });
+    $('#modal_access_control').modal({ show: true });
+}
+
+function add_group() {
+    url = '/manage/groups/add/modal' + case_param();
+    $('#modal_access_control').load(url, function (response, status, xhr) {
+        if (status !== "success") {
+             ajax_notify_error(xhr, url);
+             return false;
+        }
+
+        $('#submit_new_group').on("click", function () {
+             clear_api_error();
+            var data_sent = $('#form_new_group').serializeObject();
+            post_request_api('/manage/groups/add', JSON.stringify(data_sent), true)
+            .done((data) => {
+                if(notify_auto_api(data)) {
+                    refresh_groups();
+                    $('#modal_access_control').modal('hide');
+                }
+            });
+        });
+        $('#modal_access_control').modal({ show: true });
+    });
+}
+
+function delete_group(id) {
+
+    swal({
+      title: "Are you sure?",
+      text: "You won't be able to revert this !",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    })
+    .then((willDelete) => {
+      if (willDelete) {
+        get_request_api('/manage/groups/delete/' + id)
+        .done((data) => {
+            if(notify_auto_api(data)) {
+                refresh_groups();
+                $('#modal_access_control').modal('hide');
+            }
+        });
+      } else {
+        swal("Pfew, that was close");
+      }
+    });
+}
+
+function remove_members_from_group(group_id, user_id) {
+
+    swal({
+      title: "Are you sure?",
+      text: "This will remove the user from the group",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, remove it!'
+    })
+    .then((willDelete) => {
+        if (willDelete) {
+            url = '/manage/groups/' + group_id + '/members/delete/' + user_id;
+
+            get_request_api(url)
+            .done((data) => {
+                if(notify_auto_api(data)) {
+                    refresh_groups();
+                    refresh_group_members(group_id);
+                }
+            });
+        }
+    });
+
+}
+
+function add_members_to_group(group_id) {
+    url = 'groups/' + group_id + '/members/modal' + case_param();
+    $('#modal_ac_additional').load(url, function (response, status, xhr) {
+        if (status !== "success") {
+             ajax_notify_error(xhr, url);
+             return false;
+        }
+
+        $('#save_group_members').on("click", function () {
+            clear_api_error();
+
+            var data_sent = Object();
+            data_sent['group_members'] = $('#group_members').val();
+            data_sent['csrf_token'] = $('#csrf_token').val();
+
+            post_request_api('groups/' + group_id + '/members/update', JSON.stringify(data_sent), true)
+            .done((data) => {
+                if(notify_auto_api(data)) {
+                    refresh_groups();
+                    refresh_group_members(group_id);
+                    $('#modal_ac_additional').modal('hide');
+                }
+            });
+
+            return false;
+        });
+        $('#modal_ac_additional').modal({ show: true });
+    });
+}
+
+function refresh_group_members(group_id) {
+    if (modal_group_table !== undefined) {
+        get_request_api('/manage/groups/' + group_id)
+        .done((data) => {
+            if(notify_auto_api(data)) {
+                modal_group_table.clear();
+                modal_group_table.rows.add(data.data.group_members).draw();
+            }
+        });
+    }
+}
