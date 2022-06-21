3ef51153baf3e1dba20f4ef026123317c3c0f9ce
whikernel <whitekernel@whitekernel.fr>
2022-06-21 07:54:23 +0200

    [#49][ADD] Template for user orgs membership from user modal

--- source/app/static/assets/js/iris/manage_users.js
+++ source/app/static/assets/js/iris/manage_users.js
@@ -218,6 +218,34 @@ function manage_user_groups(user_id) {
     });
 }
 
+
+function manage_user_organisations(user_id) {
+    url = 'users/' + user_id + '/organisations/modal' + case_param();
+    $('#modal_access_control').load(url, function (response, status, xhr) {
+        if (status !== "success") {
+             ajax_notify_error(xhr, url);
+             return false;
+        }
+        $('#modal_access_control').modal({ show: true });
+
+        $('#save_user_orgs_membership').on("click", function () {
+            clear_api_error();
+
+            var data_sent = Object();
+            data_sent['orgs_membership'] = $('#user_orgs_membership').val();
+            data_sent['csrf_token'] = $('#csrf_token').val();
+
+            post_request_api('/manage/users/' + user_id + '/organisations/update', JSON.stringify(data_sent), true)
+            .done((data) => {
+                if(notify_auto_api(data)) {
+                    user_detail(user_id, 'user_orgs_tab');
+                }
+            });
+        });
+    });
+}
+
+
 $(document).ready(function () {
     refresh_users();
 });
