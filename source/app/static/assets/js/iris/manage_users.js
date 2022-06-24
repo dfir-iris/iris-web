dd51c75f4acba312820ad683b122e83749761bf2
whikernel <whitekernel@whitekernel.fr>
2022-06-24 11:40:42 +0200

    [#49][FIX] Loading button text in modal cac

--- source/app/static/assets/js/iris/manage_users.js
+++ source/app/static/assets/js/iris/manage_users.js
@@ -264,7 +264,7 @@ function manage_user_cac(user_id) {
     $('#manage_user_cac_button').text('Loading manager...');
 
     $('#modal_ac_additional').load(url, function (response, status, xhr) {
-        $('#manage_org_cac_button').text('Grant case access');
+        $('#manage_user_cac_button').text('Grant case access');
         if (status !== "success") {
              ajax_notify_error(xhr, url);
              return false;
