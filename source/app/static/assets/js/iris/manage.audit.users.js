211b6da0ed137015848ac960d37b0120c9d55ce6
whikernel <whitekernel@whitekernel.fr>
2022-06-26 18:11:42 +0200

    [#49][ADD] Selector of users for audit

--- source/app/static/assets/js/iris/manage.audit.users.js
+++ source/app/static/assets/js/iris/manage.audit.users.js
@@ -1,9 +1,51 @@
 
-url = '/manage/access-control/audit/users/9/modal' + case_param()
-$('#user_audit_content').load(url, function (response, status, xhr) {
-    hide_minimized_modal_box();
-    if (status !== "success") {
-         ajax_notify_error(xhr, url);
-         return false;
+
+
+function get_user_audit_page() {
+
+    us_val = $('#users_audit_select').val();
+    if (!us_val) {
+        notify_error('I really wanna you but I still can\'t read your mind');
+        return false;
     }
+
+    $('#get_user_audit_btn').text('Auditing user..');
+    url = '/manage/access-control/audit/users/'+ us_val +'/modal' + case_param();
+    $('#user_audit_content').load(url, function (response, status, xhr) {
+        $('#get_user_audit_btn').text('Audit');
+        if (status !== "success") {
+             $('#get_user_audit_btn').text('Audit');
+             ajax_notify_error(xhr, url);
+             return false;
+        }
+    });
+}
+
+function refresh_users_list_audit() {
+    get_request_api('/manage/users/list')
+    .done((data) => {
+
+        if(notify_auto_api(data, true)) {
+
+            $('#users_audit_select').selectpicker({
+                liveSearch: true,
+                title: "Select user to audit",
+                style: "btn-outline-white",
+                size: 10
+            });
+            data_select = [];
+            for (user in data.data) {
+                label = `${data.data[user].user_login} (${data.data[user].user_name} - ${data.data[user].user_email})`;
+                $("#users_audit_select").append('<option value="'+data.data[user].user_id+'">'+label+'</option>');
+            }
+            $("#users_audit_select").selectpicker("refresh");
+            $("#users_audit_select").show();
+
+        }
+    });
+}
+
+$(document).ready(function () {
+    refresh_users_list_audit();
 });
+
