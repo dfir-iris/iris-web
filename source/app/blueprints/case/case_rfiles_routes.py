9d5d5fbefbd1c39e59c6b5a2a39eb2134f6943a4
whikernel <whitekernel@whitekernel.fr>
2022-07-03 16:03:43 +0200

    [#49][FIX] very long modal title in ioc

--- source/app/blueprints/case/case_rfiles_routes.py
+++ source/app/blueprints/case/case_rfiles_routes.py
@@ -142,7 +142,7 @@ def case_edit_rfile_modal(cur_id, caseid, url_redir):
 
 
 @case_rfiles_blueprint.route('/case/evidences/add/modal', methods=['GET'])
-@ac_api_case_requires(CaseAccessLevel.full_access)
+@ac_case_requires(CaseAccessLevel.full_access)
 def case_add_rfile_modal(caseid, url_redir):
     if url_redir:
         return redirect(url_for('case_rfiles.case_rfile', cid=caseid, redirect=True))
