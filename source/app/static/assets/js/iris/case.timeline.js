a48fd0c358b7713193bbf1212f7c05255c0b400b
ekt0 <ekt0plasm@protonmail.com>
2022-09-03 13:59:53 +0200

    [#121][FIX] solve wrong event flash at duplication

--- source/app/static/assets/js/iris/case.timeline.js
+++ source/app/static/assets/js/iris/case.timeline.js
@@ -54,6 +54,10 @@ function duplicate_event(id) {
     get_request_api("timeline/events/duplicate/" + id)
     .done((data) => {
         if(notify_auto_api(data)) {
+            if ("data" in data && "event_id" in data.data)
+            {
+                window.location.hash = data.data.event_id;
+            }
             apply_filtering();
         }
     });
