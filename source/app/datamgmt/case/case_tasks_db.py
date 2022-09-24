f5d2573c6b0d0372cd9b73c795ec38ae18c0ec09
whikernel <whitekernel@whitekernel.fr>
2022-09-24 10:50:10 +0200

    [#66][ADD] Task UUID in task info

--- source/app/datamgmt/case/case_tasks_db.py
+++ source/app/datamgmt/case/case_tasks_db.py
@@ -37,6 +37,7 @@ def get_tasks_status():
 def get_tasks(caseid):
     return CaseTasks.query.with_entities(
         CaseTasks.id.label("task_id"),
+        CaseTasks.task_uuid,
         CaseTasks.task_title,
         CaseTasks.task_description,
         CaseTasks.task_open_date,
