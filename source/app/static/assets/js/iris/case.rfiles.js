68d81d7296fc5290c2d7199fb98ae36a755df65b
whikernel <whitekernel@whitekernel.fr>
2022-09-25 10:09:52 +0200

    [#106][ADD] Deletion confirmation prompt for evidences

--- source/app/static/assets/js/iris/case.rfiles.js
+++ source/app/static/assets/js/iris/case.rfiles.js
@@ -179,12 +179,16 @@ function update_rfile(rfiles_id) {
 
 /* Delete an rfiles */
 function delete_rfile(rfiles_id) {
-
-    get_request_api('evidences/delete/' + rfiles_id)
-    .done(function(data){
-       reload_rfiles();
-       $('#modal_add_rfiles').modal('hide');
-       notify_auto_api(data);
+    do_deletion_prompt("You are about to delete evidence #" + rfiles_id)
+    .then((doDelete) => {
+        if (doDelete) {
+            get_request_api('evidences/delete/' + rfiles_id)
+            .done(function(data){
+               reload_rfiles();
+               $('#modal_add_rfiles').modal('hide');
+               notify_auto_api(data);
+            });
+        }
     });
 }
 
