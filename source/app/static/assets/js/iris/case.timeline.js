2e52d53f7b46264d6afa785a9ec18eeab9eeff70
whikernel <whitekernel@whitekernel.fr>
2022-10-30 08:08:33 +0100

    [#124][ADD] Markdown support in comments

--- source/app/static/assets/js/iris/case.timeline.js
+++ source/app/static/assets/js/iris/case.timeline.js
@@ -974,16 +974,24 @@ function load_comments(event_id) {
         if (notify_auto_api(data, true)) {
             $('#comments_list').empty();
             for (var i = 0; i < data['data'].length; i++) {
+
+                comment_text = data['data'][i].comment_text;
+                converter = new showdown.Converter({
+                    tables: true,
+                    parseImgDimensions: true
+                });
+                html = converter.makeHtml(comment_text);
+                comment_html = filterXSS(html);
                 comment = `
                     <div class="row mb-3">
                         <div class="col-12">
                             <div class="d-flex">
                                 <div class="avatar avatar-online">
-                                    <span class="avatar-title rounded-circle border border-white bg-info">${data['data'][i].name.charAt(0)}</span>
+                                    <span class="avatar-title rounded-circle border border-white bg-info">${data['data'][i].name.charAt(0).toUpperCase()}</span>
                                 </div>
                                 <div class="flex-1 ml-3 pt-1">
                                     <h6 class="text-uppercase fw-bold mb-1">${data['data'][i].name}</h6>
-                                    <span class="text-muted">${data['data'][i].comment_text}</span>
+                                    <span class="text-muted">${comment_html}</span>
                                 </div>
                                 <div class="float-right pt-1">
                                     <small class="text-muted">${data['data'][i].comment_date}</small>
