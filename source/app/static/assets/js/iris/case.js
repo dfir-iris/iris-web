cccef569ccc73c829adafa43ed390757652a29b8
whikernel <whitekernel@whitekernel.fr>
2022-11-08 20:53:12 +0100

    [ADD] Random ID on protagonist additions

--- source/app/static/assets/js/iris/case.js
+++ source/app/static/assets/js/iris/case.js
@@ -111,7 +111,9 @@ function close_case(id) {
 }
 
 function add_protagonist() {
+    random_string = Math.random().toString(36).substring(7);
     prota_html = $('#protagonist_list_edit_template').html();
+    prota_html = prota_html.replace(/__PROTAGONIST_ID__/g, random_string);
     $('#protagonist_list_edit').append(prota_html);
 }
 
