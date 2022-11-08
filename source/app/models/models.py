e11f05ab082457a23f60ad119bb1083fc67d09d2
whikernel <whitekernel@whitekernel.fr>
2022-11-08 18:32:50 +0100

    [ADD] Case status

--- source/app/models/models.py
+++ source/app/models/models.py
@@ -19,6 +19,8 @@
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 # IMPORTS ------------------------------------------------
+import enum
+
 import secrets
 import uuid
 
@@ -48,6 +50,12 @@ from app import db
 
 Base = declarative_base()
 metadata = Base.metadata
+
+
+class CaseStatus(enum.Enum):
+    false_positive = 0x1
+    true_positive = 0x2
+    unknown = 0x4
 
 
 def create_safe(session, model, **kwargs):
