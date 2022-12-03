997fd008d2b0f4abc2937eda81a14ca7d89bb1e2
whikernel <whitekernel@whitekernel.fr>
2022-12-03 16:46:38 +0100

    [ADD] Ldap password to filter

--- source/app/iris_engine/access_control/ldap_handler.py
+++ source/app/iris_engine/access_control/ldap_handler.py
@@ -33,6 +33,7 @@ def ldap_authenticate(ldap_user_name, ldap_user_pwd):
     Authenticate to the LDAP server
     """
     ldap_user_name = conv.escape_filter_chars(ldap_user_name)
+    ldap_user_pwd = conv.escape_filter_chars(ldap_user_pwd)
     ldap_user = f"{app.config.get('LDAP_USER_PREFIX')}{ldap_user_name.strip()},{app.config.get('LDAP_USER_SUFFIX')}"
 
     tls_configuration = Tls(validate=ssl.CERT_REQUIRED,
