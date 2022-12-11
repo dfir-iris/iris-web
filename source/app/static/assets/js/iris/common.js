6165f065426c6535a4a51d0cd204c1d66e16f052
whikernel <whitekernel@whitekernel.fr>
2022-12-11 16:27:05 +0100

    [FIX] Missing customer in context switcher

--- source/app/static/assets/js/iris/common.js
+++ source/app/static/assets/js/iris/common.js
@@ -1127,9 +1127,9 @@ function load_context_switcher() {
                 case_name = sanitizeHTML(ocs[index].name);
                 cs_name = sanitizeHTML(ocs[index].customer_name);
                 if (ocs[index].close_date != null) {
-                    $('#switch_case_closed_opt').append(`<option value="${ocs[index].case_id}">${case_name} (cs_name) ${ocs[index].access}</option>`);
+                    $('#switch_case_closed_opt').append(`<option value="${ocs[index].case_id}">${case_name} (${cs_name}) ${ocs[index].access}</option>`);
                 } else {
-                    $('#switch_case_opened_opt').append(`<option value="${ocs[index].case_id}">${case_name} (cs_name) ${ocs[index].access}</option>`)
+                    $('#switch_case_opened_opt').append(`<option value="${ocs[index].case_id}">${case_name} (${cs_name}) ${ocs[index].access}</option>`)
                 }
             }
 
