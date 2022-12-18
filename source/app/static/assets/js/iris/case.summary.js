27f3bd2f2acc4ebd31f20ebb3770908f8e56e110
whikernel <whitekernel@whitekernel.fr>
2022-12-18 15:41:31 +0100

    [FIX] Update of AC from case management

--- source/app/static/assets/js/iris/case.summary.js
+++ source/app/static/assets/js/iris/case.summary.js
@@ -295,34 +295,6 @@ function manage_case(case_id) {
 }
 
 
-function view_case_access_via_group(case_id) {
-    url = '/case/groups/access/modal' + case_param();
-    $('#modal_ac_additional').load(url, function (response, status, xhr) {
-        if (status !== "success") {
-             ajax_notify_error(xhr, url);
-             return false;
-        }
-        $('#modal_ac_additional').modal({ show: true });
-    });
-}
-
-function set_case_access_via_group(case_id) {
-    var data = {
-        "case_id": parseInt(case_id),
-        "access_level": parseInt($('#group_case_ac_select').val()),
-        "group_id": parseInt($('#group_case_access_select').val()),
-        "csrf_token": $('#csrf_token').val()
-    };
-
-    post_request_api('/case/access/set-group', JSON.stringify(data))
-    .done((data) => {
-        notify_auto_api(data);
-        access_case_info_reload(case_id);
-        $('#modal_ac_additional').modal('hide');
-    });
-
-}
-
 $(document).ready(function() {
 
     if ($("#editor_summary").attr("data-theme") != "dark") {
