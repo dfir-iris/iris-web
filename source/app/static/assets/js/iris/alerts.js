e585f8cf53a549a3420bf252a5767f16918bf575
whikernel <whitekernel@whitekernel.fr>
2023-04-06 18:00:43 +0200

    [ADD] Refresh alerts buttons

--- source/app/static/assets/js/iris/alerts.js
+++ source/app/static/assets/js/iris/alerts.js
@@ -618,6 +618,20 @@ $('#orderAlertsBtn').on('click', function () {
   updateAlerts(page_number, per_page, filters);
 });
 
+function refreshAlerts(){
+    const queryParams = new URLSearchParams(window.location.search);
+    let page_number = parseInt(queryParams.get('page'));
+    let per_page = parseInt(queryParams.get('per_page'));
+
+    const formData = new FormData($('#alertFilterForm')[0]);
+    const filters = Object.fromEntries(formData.entries());
+
+    updateAlerts(page_number, per_page, filters)
+        .then(() => {
+            notify_success('Refreshed');
+        });
+}
+
 $('#alertFilterForm').on('submit', (e) => {
   e.preventDefault();
 
