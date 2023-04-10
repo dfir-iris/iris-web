08ed563c5c05f2ed03b73a6dfe83f5b0f9aab614
ekt0 <ekt0plasm@protonmail.com>
2023-04-10 15:15:14 +0200

    [#192][ADD] Upload a case template

--- source/app/static/assets/js/iris/manage.case_templates.js
+++ source/app/static/assets/js/iris/manage.case_templates.js
@@ -257,3 +257,48 @@ function update_case_template(ctempl_id, editor, partial, complete){
 
     return false;
 }
+
+function fire_upload_case_template() {
+    $('#modal_upload_case_template').modal('show');
+}
+
+function upload_case_template() {
+
+    var file = $("#input_upload_case_template").get(0).files[0];
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        fileData = e.target.result
+        var data = new Object();
+        data['csrf_token'] = $('#csrf_token').val();
+        data['case_template_json'] = fileData;
+
+        post_request_api('/manage/case-templates/add', JSON.stringify(data), false, function() {
+            window.swal({
+                  title: "Adding...",
+                  text: "Please wait",
+                  icon: "/static/assets/img/loader.gif",
+                  button: false,
+                  allowOutsideClick: false
+            });
+        })
+       .done((data) => {
+            jsdata = data;
+            if (jsdata.status == "success") {
+                refresh_case_template_table();
+                $('#modal_upload_case_template').modal('hide');
+                swal("Got news for you", data.message, "success");
+
+            } else {
+                swal("Got bad news for you", data.message, "error");
+            }
+        })
+        .always((data) => {
+            $("#input_upload_case_template").val("");
+            window.swal.close();
+        });
+
+    };
+    reader.readAsText(file)
+
+    return false;
+}
