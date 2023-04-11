fbbbdebfe0e5464d108416efad4c548da8f4aafe
whikernel <whitekernel@whitekernel.fr>
2023-04-11 14:30:16 +0200

    [ADD] Deletion permission for alerts

--- source/app/blueprints/alerts/alerts_routes.py
+++ source/app/blueprints/alerts/alerts_routes.py
@@ -49,7 +49,7 @@ alerts_blueprint = Blueprint(
 
 # CONTENT ------------------------------------------------
 @alerts_blueprint.route('/alerts/filter', methods=['GET'])
-@ac_api_requires(Permissions.alerts_reader)
+@ac_api_requires(Permissions.alerts_read)
 def alerts_list_route(caseid) -> Response:
     """
     Get a list of alerts from the database
@@ -99,7 +99,7 @@ def alerts_list_route(caseid) -> Response:
 
 
 @alerts_blueprint.route('/alerts/add', methods=['POST'])
-@ac_api_requires(Permissions.alerts_writer)
+@ac_api_requires(Permissions.alerts_write)
 def alerts_add_route(caseid) -> Response:
     """
     Add a new alert to the database
@@ -145,7 +145,7 @@ def alerts_add_route(caseid) -> Response:
 
 
 @alerts_blueprint.route('/alerts/<int:alert_id>', methods=['GET'])
-@ac_api_requires(Permissions.alerts_reader)
+@ac_api_requires(Permissions.alerts_read)
 def alerts_get_route(caseid, alert_id) -> Response:
     """
     Get an alert from the database
@@ -177,7 +177,7 @@ def alerts_get_route(caseid, alert_id) -> Response:
 
 
 @alerts_blueprint.route('/alerts/similarities/<int:alert_id>', methods=['GET'])
-@ac_api_requires(Permissions.alerts_reader)
+@ac_api_requires(Permissions.alerts_read)
 def alerts_similarities_route(caseid, alert_id) -> Response:
     """
     Get an alert and similarities from the database
@@ -206,7 +206,7 @@ def alerts_similarities_route(caseid, alert_id) -> Response:
 
 
 @alerts_blueprint.route('/alerts/update/<int:alert_id>', methods=['POST'])
-@ac_api_requires(Permissions.alerts_writer)
+@ac_api_requires(Permissions.alerts_write)
 def alerts_update_route(alert_id, caseid) -> Response:
     """
     Update an alert in the database
@@ -246,7 +246,7 @@ def alerts_update_route(alert_id, caseid) -> Response:
 
 
 @alerts_blueprint.route('/alerts/batch/update', methods=['POST'])
-@ac_api_requires(Permissions.alerts_writer)
+@ac_api_requires(Permissions.alerts_write)
 def alerts_batch_update_route(caseid: int) -> Response:
     """
     Update multiple alerts in the database
@@ -294,7 +294,7 @@ def alerts_batch_update_route(caseid: int) -> Response:
 
 
 @alerts_blueprint.route('/alerts/delete/<int:alert_id>', methods=['POST'])
-@ac_api_requires(Permissions.alerts_writer)
+@ac_api_requires(Permissions.alerts_delete)
 def alerts_delete_route(alert_id, caseid) -> Response:
     """
     Delete an alert from the database
@@ -325,7 +325,7 @@ def alerts_delete_route(alert_id, caseid) -> Response:
 
 
 @alerts_blueprint.route('/alerts/escalate/<int:alert_id>', methods=['POST'])
-@ac_api_requires(Permissions.alerts_writer)
+@ac_api_requires(Permissions.alerts_write)
 def alerts_escalate_route(alert_id, caseid) -> Response:
     """
     Escalate an alert
@@ -385,7 +385,7 @@ def alerts_escalate_route(alert_id, caseid) -> Response:
 
 
 @alerts_blueprint.route('/alerts/merge/<int:alert_id>', methods=['POST'])
-@ac_api_requires(Permissions.alerts_writer)
+@ac_api_requires(Permissions.alerts_write)
 def alerts_merge_route(alert_id, caseid) -> Response:
     """
     Merge an alert into a case
@@ -438,7 +438,7 @@ def alerts_merge_route(alert_id, caseid) -> Response:
 
 
 @alerts_blueprint.route('/alerts/unmerge/<int:alert_id>', methods=['POST'])
-@ac_api_requires(Permissions.alerts_writer)
+@ac_api_requires(Permissions.alerts_write)
 def alerts_unmerge_route(alert_id, caseid) -> Response:
     """
     Unmerge an alert from a case
@@ -478,7 +478,7 @@ def alerts_unmerge_route(alert_id, caseid) -> Response:
 
 
 @alerts_blueprint.route('/alerts', methods=['GET'])
-@ac_requires(Permissions.alerts_reader)
+@ac_requires(Permissions.alerts_read)
 def alerts_list_view_route(caseid, url_redir) -> Union[str, Response]:
     """
     List all alerts
