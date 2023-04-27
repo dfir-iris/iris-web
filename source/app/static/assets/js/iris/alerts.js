bcb3ff42ec9ff3f4ca546433c7558dc128cf36ae
whikernel <whitekernel@whitekernel.fr>
2023-04-27 09:44:08 +0200

    [ADD] Deletion prompt for unitary deletion

--- source/app/static/assets/js/iris/alerts.js
+++ source/app/static/assets/js/iris/alerts.js
@@ -1229,12 +1229,17 @@ function showEnrichment(enrichment) {
 }
 
 function delete_alert(alert_id) {
-    post_request_api('/alerts/delete/'+alert_id)
-    .then(function (data) {
-        if (notify_auto_api(data)) {
-            setFormValuesFromUrl();
-        }
-    });
+    do_deletion_prompt(`Are you sure you want to delete alert #${alert_id}?`, true)
+        .then((doDelete) => {
+            if (doDelete) {
+                post_request_api(`/alerts/delete/${alert_id}`)
+                    .then((data) => {
+                        if (notify_auto_api(data)) {
+                            setFormValuesFromUrl();
+                        }
+                    });
+            }
+        });
 }
 
 async function editAlert(alert_id, close=false) {
