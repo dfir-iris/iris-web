62a09cf3250e5899ff1f3d6c6407a22e9bdca6de
whikernel <whitekernel@whitekernel.fr>
2023-07-23 15:50:39 +0200

    Bump version: 2.3.0 → 2.3.1

--- source/app/configuration.py
+++ source/app/configuration.py
@@ -265,7 +265,7 @@ class CeleryConfig:
 # --------- APP ---------
 class Config:
     # Handled by bumpversion
-    IRIS_VERSION = "v2.3.0"
+    IRIS_VERSION = "v2.3.1"
 
     API_MIN_VERSION = "2.0.0"
     API_MAX_VERSION = "2.0.3"
