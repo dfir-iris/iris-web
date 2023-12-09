cfbf0f74e710d1ab0a38b87fa4df1396907889a6
Ektoplasma <ekt0plasm@protonmail.com>
2023-12-09 17:27:56 +0100

    [IMP] case and manage case javascript refactoring

---
+++ source/app/static/assets/js/iris/case.common.js
@@ -0,0 +1,30 @@
+function buildShareLink(lookup_id) {
+    current_path = location.protocol + '//' + location.host + location.pathname;
+    current_path = current_path + case_param() + '&shared=' + lookup_id;
+
+    return current_path;
+}
+
+function getSharedLink(){
+    queryString = window.location.search;
+    urlParams = new URLSearchParams(queryString);
+    if (Number.isInteger(parseInt(urlParams.get('shared')))) {
+        return urlParams.get('shared')
+    }
+    return null;
+}
+
+$(document).ready(function(){
+    $(function(){
+        var current = location.pathname;
+        $('#h_nav_tab li').each(function(){
+            var $this = $(this);
+            var child = $this.children();
+            // if the current path is like this link, make it active
+            if(child.attr('href') !== undefined && child.attr('href').split("?")[0] == current){
+                $this.addClass('active');
+                return;
+            }
+        })
+    });
+});
