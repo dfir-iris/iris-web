b7c59f135ecedf2f75abdbd9c28a6f983b426271
whikernel <whitekernel@whitekernel.fr>
2023-12-22 10:51:40 +0100

    [ADD] Code to add new case from anywhere

--- source/app/static/assets/js/iris/common.js
+++ source/app/static/assets/js/iris/common.js
@@ -1338,6 +1338,79 @@ function load_add_case() {
     });
 }
 
+/* Submit event handler for new case */
+function submit_new_case() {
+
+    let data_sent = $('form#form_new_case').serializeObject();
+    let ret = get_custom_attributes_fields();
+    let has_error = ret[0].length > 0;
+    let attributes = ret[1];
+
+    if (has_error){return false;}
+
+    data_sent['custom_attributes'] = attributes;
+
+    send_add_case(data_sent);
+
+    return false;
+}
+
+
+function send_add_case(data_sent) {
+
+    post_request_api('/manage/cases/add', JSON.stringify(data_sent), true, function () {
+        $('#submit_new_case_btn').text('Checking data..')
+            .attr("disabled", true)
+            .removeClass('bt-outline-success')
+            .addClass('btn-success', 'text-dark');
+    })
+    .done((data) => {
+        if (notify_auto_api(data, true)) {
+            let case_id = data.data.case_id;
+            swal("That's done !",
+                "Case has been successfully created",
+                "success",
+                {
+                    buttons: {
+                        dash: {
+                            text: "Go to dashboard",
+                            value: "dash",
+                            color: '#d33'
+                        },
+                        go_case: {
+                            text: "Switch to newly created case",
+                            value: "go_case"
+                        }
+                    }
+                }
+            ).then((value) => {
+                switch (value) {
+
+                    case "dash":
+                        window.location.replace("/dashboard" + case_param());
+                        break;
+
+                    case 'go_case':
+                        window.location.replace("/case?cid=" + case_id);
+
+                    default:
+                        window.location.replace("/case?cid=" + case_id);
+                }
+            });
+        }
+    })
+    .always(() => {
+        $('#submit_new_case_btn')
+        .attr("disabled", false)
+        .addClass('bt-outline-success')
+        .removeClass('btn-success', 'text-dark');
+    })
+    .fail(() => {
+        $('#submit_new_case_btn').text('Save');
+    })
+
+}
+
 function load_context_switcher() {
 
     var options = {
