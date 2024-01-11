1e137b0e0bc3387a735ee0c48944fdfc14d65195
whikernel <whitekernel@whitekernel.fr>
2024-01-11 09:50:51 +0100

    [FIX] Migration with updated sqlalchemy

--- source/app/alembic/versions/e0df2de997cc_add_namespace_to_tags.py
+++ source/app/alembic/versions/e0df2de997cc_add_namespace_to_tags.py
@@ -7,6 +7,7 @@ Create Date: 2023-12-30 09:24:32.991449
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import text
 
 from app.alembic.alembic_utils import _table_has_column
 from app.datamgmt.manage.manage_tags_db import add_db_tag
@@ -25,32 +26,40 @@ def upgrade():
 
     tags = set()
     # Parse all tags from iocs and register them into tags if they don't exists
-    ioc_tags = op.get_bind().execute("SELECT ioc_tags FROM ioc;").fetchall()
+    ioc_tags = op.get_bind().execute(text("SELECT ioc_tags FROM ioc;")).fetchall()
     for entry in ioc_tags:
         for ioc_tag in entry[0].split(',') if entry[0] else []:
-            tags.add(ioc_tag.strip())
+            ioc_tag = ioc_tag.strip()
+            if ioc_tag:
+                tags.add(ioc_tag)
 
     # Parse all tags from assets and register them into tags if they don't exists
-    asset_tags = op.get_bind().execute("SELECT asset_tags FROM case_assets;").fetchall()
+    asset_tags = op.get_bind().execute(text("SELECT asset_tags FROM case_assets;")).fetchall()
     for entry in asset_tags:
         for asset_tag in entry[0].split(',') if entry[0] else []:
-            tags.add(asset_tag.strip())
+            asset_tag = asset_tag.strip()
+            if asset_tag:
+                tags.add(asset_tag.strip())
 
     # Parse all tags from case tasks and register them into tags if they don't exists
-    task_tags = op.get_bind().execute("SELECT task_tags FROM case_tasks;").fetchall()
+    task_tags = op.get_bind().execute(text("SELECT task_tags FROM case_tasks;")).fetchall()
     for entry in task_tags:
         for task_tag in entry[0].split(',') if entry[0] else []:
-            tags.add(task_tag.strip())
+            task_tag = task_tag.strip()
+            if task_tag:
+                tags.add(task_tag.strip())
 
     # Parse all tags from events and register them into tags if they don't exists
-    event_tags = op.get_bind().execute("SELECT event_tags FROM cases_events;").fetchall()
+    event_tags = op.get_bind().execute(text("SELECT event_tags FROM cases_events;")).fetchall()
     for entry in event_tags:
         for event_tag in entry[0].split(',') if entry[0] else []:
-            tags.add(event_tag.strip())
+            event_tag = event_tag.strip()
+            if event_tag:
+                tags.add(event_tag.strip())
 
-    # Add all tags to the database if they don't exist
-    for tag in tags:
-        add_db_tag(tag)
+    # Bulk add all tags with a RAW SQL query and skip error if tag already exists
+    op.get_bind().execute(text("INSERT INTO tags (tag_title) VALUES (:tag_title) ON CONFLICT (tag_title) DO NOTHING"),
+                          [{"tag_title": tag} for tag in tags])
 
     pass
 
