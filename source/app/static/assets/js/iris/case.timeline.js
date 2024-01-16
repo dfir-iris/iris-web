36bd9facdc5b45825f2929453723620fecaca595
whikernel <whitekernel@whitekernel.fr>
2024-01-16 10:13:37 +0100

    [FIX] Toggle child in timeline

--- source/app/static/assets/js/iris/case.timeline.js
+++ source/app/static/assets/js/iris/case.timeline.js
@@ -979,11 +979,33 @@ function toggle_child_events() {
     let child_events = $('.timeline-child');
     if (child_events.is(':visible')) {
         child_events.hide();
-        child_events.prev().find('button:last').html('<span class="btn-label"><i class="fa fa-chevron-right"></i> Child events</span>');
+        // Find the button of the parent event, excluding the child events themselves
+        for (let i = 0; i < child_events.length; i++) {
+            let child_event = child_events[i];
+            let parent_event = $(child_event).prev();
+            if (parent_event.hasClass('timeline-child')) {
+                continue;
+            }
+            let btn = parent_event.find('button:last');
+            if (btn.html().indexOf('fa-chevron-down') !== -1) {
+                btn.html('<span class="btn-label"><i class="fa fa-chevron-right"></i> Child events</span>');
+            }
+        }
+
 
     } else {
         child_events.show();
-        child_events.prev().find('button:last').html('<span class="btn-label"><i class="fa fa-chevron-down"></i></span>');
+        for (let i = 0; i < child_events.length; i++) {
+            let child_event = child_events[i];
+            let parent_event = $(child_event).prev();
+            if (parent_event.hasClass('timeline-child')) {
+                continue;
+            }
+            let btn = parent_event.find('button:last');
+            if (btn.html().indexOf('fa-chevron-right') !== -1) {
+                btn.html('<span class="btn-label"><i class="fa fa-chevron-down"></i></span>');
+            }
+        }
     }
 }
 
