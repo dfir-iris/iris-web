7d3adf8969ef753623f893c687b21056b3a0221f
whikernel <whitekernel@whitekernel.fr>
2024-01-16 11:53:43 +0100

    [#372][FIX] Fixed issue of JSOn rendering in descriptions

--- source/app/static/assets/js/iris/common.js
+++ source/app/static/assets/js/iris/common.js
@@ -72,10 +72,11 @@ function ellipsis_field( data, cutoff, wordbreak ) {
 
 function ret_obj_dt_description(data) {
     let anchor = $('<span>');
+    let dataContent = typeof data === 'object' ? JSON.stringify(data) : data;
     anchor.attr('data-toggle', 'popover')
         .attr('data-trigger', 'hover')
         .attr('title', 'Description')
-        .attr('data-content', data)
+        .attr('data-content', dataContent)
         .attr('href', '#')
         .css('cursor', 'pointer')
         .text(ellipsis_field_raw(data, 64));
