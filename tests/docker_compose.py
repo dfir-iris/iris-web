ba1bb077234bc574c7d9e87b5b89d9f7e2336403
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-02-06 16:12:46 +0100

    [ADD] First API test

---
+++ tests/docker_compose.py
@@ -0,0 +1,34 @@
+#  IRIS Source Code
+#  Copyright (C) 2021 - Airbus CyberSecurity (SAS)
+#  ir@cyberactionlab.net
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import subprocess
+
+
+class DockerCompose:
+
+    def __init__(self, docker_compose_path):
+        self._docker_compose_path = docker_compose_path
+
+    def start(self):
+        subprocess.run(['docker-compose', 'up', '--detach'], cwd=self._docker_compose_path)
+
+    def extract_all_logs(self):
+        return subprocess.check_output(['docker-compose', 'logs', '--no-color'], cwd=self._docker_compose_path, universal_newlines=True)
+    def stop(self):
+        subprocess.run(['docker-compose', 'down'], cwd=self._docker_compose_path)
+        subprocess.run(['docker', 'volume', 'prune', '--force'])
---
+++ tests/rest_api.py
@@ -0,0 +1,50 @@
+#  IRIS Source Code
+#  Copyright (C) 2021 - Airbus CyberSecurity (SAS)
+#  ir@cyberactionlab.net
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import requests
+from requests.exceptions import ConnectionError
+from urllib import parse
+
+
+class RestApi:
+
+    def __init__(self, url, api_key):
+        self._url = url
+        self._headers = {'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'}
+
+    def _build_url(self, path):
+        return parse.urljoin(self._url, path)
+
+    def get(self, path, query_parameters=None):
+        url = self._build_url(path)
+        response = requests.get(url, headers=self._headers, params=query_parameters)
+        print(f'GET {url} => {response.status_code}')
+        return response
+
+    def post(self, path, payload):
+        url = self._build_url(path)
+        response = requests.post(url, headers=self._headers, json=payload)
+        print(f'POST {url} {payload} => {response.status_code}')
+        return response
+
+    def is_ready(self):
+        try:
+            requests.head(self._url)
+            return True
+        except ConnectionError:
+            return False
