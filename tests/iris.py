1af8a0c1ebca827892d48c4208b81ebc710c4267
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-03-27 08:24:15 +0100

    [IMP] create a new case for ioc test so that the default case is not polluted (avoid interactions between different tests)

--- tests/iris.py
+++ tests/iris.py
@@ -84,7 +84,8 @@ class Iris:
             'case_customer': 1,
             'case_soc_id': ''
         }
-        return self._api.post('/manage/cases/add', body)
+        response = self._api.post('/manage/cases/add', body)
+        return response['data']
 
     def update_case(self, case_identifier, data):
         return self._api.post(f'/manage/cases/update/{case_identifier}', data)
