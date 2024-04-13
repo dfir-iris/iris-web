a0b52ad855f838a5d07344b12f19d71f79ee573c
Connor Payne <connorpayne1998@gmail.com>
2024-04-13 17:04:02 +0100

    Update case.notes.js

--- source/app/static/assets/js/iris/case.notes.js
+++ source/app/static/assets/js/iris/case.notes.js
@@ -21,6 +21,10 @@ const preventFormDefaultBehaviourOnSubmit = (event) => {
     return false;
 };
 
+var editor = ace.edit("editor_detail",
+    {autoScrollEditorIntoView: true,
+    minLines: 4
+    });
 
 function Collaborator( session_id, n_id ) {
     this.collaboration_socket = collaborator_socket;
@@ -357,6 +361,45 @@ async function note_detail(id) {
 
     });
 }
+
+function handle_ed_paste(event) {
+    filename = null;
+    const { items } = event.originalEvent.clipboardData;
+    for (let i = 0; i < items.length; i += 1) {
+      const item = items[i]; 
+
+      if (item.kind === 'string') {
+        item.getAsString(function (s){
+            filename = $.trim(s.replace(/\t|\n|\r/g, '')).substring(0, 40);
+        });
+      }
+
+      if (item.kind === 'file') {
+        const blob = item.getAsFile();
+		
+        if (blob !== null) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+				notify_success('The file is uploading in background. Don\'t leave the page');
+
+                if (filename === null) {
+					filename = random_filename(25);
+                }
+
+                upload_interactive_data(e.target.result, filename, function(data){
+                    url = data.data.file_url + case_param();
+                    event.preventDefault();
+                    editor.insertSnippet(`\n![${filename}](${url} =40%x40%)\n`);
+                });
+            };
+			reader.readAsDataURL(blob);
+        } else {
+            notify_error('Unsupported direct paste of this item. Use datastore to upload.');
+        }
+      }
+    }
+}
+
 
 function refresh_ppl_list() {
     $('#ppl_list_viewing').empty();
