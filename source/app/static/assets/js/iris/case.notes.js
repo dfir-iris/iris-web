3229c498fa532be400f288d6926067e03dd51ce7
whikernel <whitekernel@whitekernel.fr>
2024-04-21 10:27:22 +0200

    [FIX] Brought back notes copy paste of pictures

--- source/app/static/assets/js/iris/case.notes.js
+++ source/app/static/assets/js/iris/case.notes.js
@@ -943,6 +943,46 @@ function createDirectoryListItem(directory, directoryMap) {
 }
 
 
+function handle_ed_paste(event) {
+    let filename = null;
+    const { items } = event.originalEvent.clipboardData;
+    for (let i = 0; i < items.length; i += 1) {
+      const item = items[i];
+
+      if (item.kind === 'string') {
+        item.getAsString(function (s){
+            filename = $.trim(s.replace(/\t|\n|\r/g, '')).substring(0, 40);
+        });
+      }
+
+      if (item.kind === 'file') {
+        const blob = item.getAsFile();
+
+        if (blob !== null) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                notify_success('The file is uploading in background. Don\'t leave the page');
+
+                if (filename === null) {
+                    filename = random_filename(25);
+                }
+
+                upload_interactive_data(e.target.result, filename, function(data){
+                    url = data.data.file_url + case_param();
+                    event.preventDefault();
+                    note_editor.insertSnippet(`\n![${filename}](${url} =100%x40%)\n`);
+                });
+
+            };
+            reader.readAsDataURL(blob);
+        } else {
+            notify_error('Unsupported direct paste of this item. Use datastore to upload.');
+        }
+      }
+    }
+}
+
+
 function note_interval_pinger() {
     if (new Date() - last_ping > 2000) {
         collaborator_socket.emit('ping-note',
