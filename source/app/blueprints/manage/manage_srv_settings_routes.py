bac1d94dc8fe8b91f1899e8b83b5b06fb4a0e45b
whikernel <whitekernel@whitekernel.fr>
2024-05-29 08:17:46 +0200

    [IMP] Tracking of server settings change

--- source/app/blueprints/manage/manage_srv_settings_routes.py
+++ source/app/blueprints/manage/manage_srv_settings_routes.py
@@ -122,10 +122,11 @@ def manage_update_settings():
     try:
 
         original_settings = srv_settings_schema.dump(server_settings)
+
         new_settings = request.get_json()
 
         differences = list(diff(original_settings, new_settings))
-        changes = [difference for difference in differences if difference[0] == 'change']
+        changes = [{difference[1]: difference[2]} for difference in differences if difference[0] == 'change']
 
         srv_settings_sc = srv_settings_schema.load(request.get_json(), instance=server_settings)
         db.session.commit()
@@ -137,7 +138,6 @@ def manage_update_settings():
                 remove_periodic_update_checks()
 
         if srv_settings_sc:
-            changes = ', '.join(map(str, changes))
             track_activity(f"Server settings updated: {changes}")
             return response_success("Server settings updated", srv_settings_sc)
 
