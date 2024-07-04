2aca9e74d502efbdde707075a929ef40024d147a
Elise17 <63542039+Elise17@users.noreply.github.com>
2024-07-04 14:15:09 +0200

    [IMP] Change URL name for GET /api/v2/iocs/ioc_id

--- source/app/blueprints/rest/case/case_ioc_routes.py
+++ source/app/blueprints/rest/case/case_ioc_routes.py
@@ -87,7 +87,7 @@ def case_list_ioc(caseid):
 
 
 @case_ioc_rest_blueprint.route('/case/ioc/state', methods=['GET'])
-@endpoint_deprecated('GET', '/api/v2/cases/<int:caseid>/iocs')
+@endpoint_deprecated('GET', '/api/v2/iocs/<int:cur_id>')
 @ac_requires_case_identifier(CaseAccessLevel.read_only, CaseAccessLevel.full_access)
 @ac_api_requires()
 def case_ioc_state(caseid):
