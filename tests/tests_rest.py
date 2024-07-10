0ed85b9c455afba3d621bca727336df322b11432
Elise17 <63542039+Elise17@users.noreply.github.com>
2024-07-10 15:08:32 +0200

    [IMP] Change name of get_iocs in tests

--- tests/tests_rest.py
+++ tests/tests_rest.py
@@ -124,7 +124,7 @@ class TestsRest(TestCase):
         response = self._subject.get('/case/graph/getdata').json()
         self.assertEqual('success', response['status'])
 
-    def test_get_iocs_should_not_fail(self):
+    def test_get_ioc_should_not_fail(self):
         response = self._subject.get('/case/ioc/list').json()
         self.assertEqual('success', response['status'])
 
@@ -156,14 +156,14 @@ class TestsRest(TestCase):
         body = {"ioc_type_id": number, "ioc_tlp_id": 2, "ioc_value": "8.8.8.8", "ioc_description": "rewrw", "ioc_tags": ""}
         test = self._subject.create(f'/api/v2/cases/{case_identifier}/iocs', body).json()
         current_id = test['ioc_id']
-        response = self._subject.get_iocs(current_id)
+        response = self._subject.get_ioc(current_id)
         self.assertEqual(number, response['ioc_type_id'])
 
     def test_get_ioc_with_missing_ioc_identifier_should_return_error(self):
         case_identifier = self._subject.create_dummy_case()
         body = {"ioc_type_id": 1, "ioc_tlp_id": 2, "ioc_value": "8.8.8.8", "ioc_description": "rewrw", "ioc_tags": ""}
         self._subject.create(f'/api/v2/cases/{case_identifier}/iocs', body).json()
-        test = self._subject.get_iocs(None)
+        test = self._subject.get_ioc(None)
         self.assertEqual('error', test['status'])
 
     def test_delete_ioc_should_return_204(self):
