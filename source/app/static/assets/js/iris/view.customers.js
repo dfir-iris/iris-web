1ab44f73672a417a1f166abea9e534ccb4d640fd
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-07-19 09:33:04 +0200

    [IMP] Removed seemingly unused method

--- source/app/static/assets/js/iris/view.customers.js
+++ source/app/static/assets/js/iris/view.customers.js
@@ -2,15 +2,6 @@ let users_table = null;
 let cases_table = null;
 let assets_table = null;
 
-function delete_contact(contact_id, customer_id) {
-    post_request_api('/manage/customers/' + customer_id + '/contacts/' + contact_id + '/delete', null, true)
-    .done((data) => {
-        if(notify_auto_api(data)) {
-            window.location.reload();
-        }
-    });
-}
-
 function edit_contact(contact_id, customer_id) {
     url = '/manage/customers/' + customer_id + '/contacts/' + contact_id + '/modal' + case_param();
     $('#modal_add_contact_content').load(url, function (response, status, xhr) {
