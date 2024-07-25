8f1b4691df21938b4777f5f51709f92b3c41f550
Elise17 <63542039+Elise17@users.noreply.github.com>
2024-07-25 09:56:28 +0200

    [FIX] Remove staticmethod

--- tests/rest_api.py
+++ tests/rest_api.py
@@ -31,8 +31,7 @@ class RestApi:
     def _build_url(self, path):
         return parse.urljoin(self._url, path)
 
-    @staticmethod
-    def _convert_response_to_string(self, response):
+    def _convert_response_to_string(response):
         try:
             return f'{response.status_code} {response.json()}'
         except JSONDecodeError:
