6a23beed6d2b41acf00e36d56a0c3dc4c7544e1e
Elise17 <63542039+Elise17@users.noreply.github.com>
2024-07-25 14:53:25 +0200

    [IMP] add staticmethod for convert_response_to_string

--- tests/rest_api.py
+++ tests/rest_api.py
@@ -31,7 +31,8 @@ class RestApi:
     def _build_url(self, path):
         return parse.urljoin(self._url, path)
 
-    def _convert_response_to_string(self, response):
+    @staticmethod
+    def _convert_response_to_string(response):
         try:
             return f'{response.status_code} {response.json()}'
         except JSONDecodeError:
