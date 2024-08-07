90bc08eeca66c00acfc0f5e06d36843f6cdb16fc
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-08-07 10:37:04 +0200

    [IMP] Using ac_requires instead of ac_api_requires for modal /manage/access-control/audit/users/<int:cur_id>/modal

--- source/app/blueprints/manage/manage_access_control.py
+++ source/app/blueprints/manage/manage_access_control.py
@@ -25,7 +25,6 @@ from app.iris_engine.access_control.utils import ac_trace_effective_user_permiss
 from app.iris_engine.access_control.utils import ac_trace_user_effective_cases_access_2
 from app.models.authorization import Permissions
 from app.util import ac_requires
-from app.util import ac_api_requires
 
 manage_ac_blueprint = Blueprint(
         'access_control',
@@ -46,8 +45,11 @@ def manage_ac_index(caseid, url_redir):
 
 
 @manage_ac_blueprint.route('/manage/access-control/audit/users/<int:cur_id>/modal', methods=['GET'])
-@ac_api_requires(Permissions.server_administrator)
-def manage_ac_audit_user_modal(cur_id):
+@ac_requires(Permissions.server_administrator, no_cid_required=True)
+def manage_ac_audit_user_modal(cur_id, caseid, url_redir):
+    if url_redir:
+        return redirect(url_for('access_control.manage_ac_index', cid=caseid))
+
     access_audit = ac_trace_user_effective_cases_access_2(cur_id)
     permissions_audit = ac_trace_effective_user_permissions(cur_id)
 
