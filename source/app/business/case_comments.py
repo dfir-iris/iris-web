4b34e4424e5daa678f63fb96aa69e3ffbaebba1a
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-08-07 13:45:39 +0200

    [IMP] Moved some code into app.business namespace

--- source/app/business/case_comments.py
+++ source/app/business/case_comments.py
@@ -1,5 +1,5 @@
 #  IRIS Source Code
-#  DFIR-IRIS Team
+#  Copyright (C) ${current_year} - DFIR-IRIS
 #  contact@dfir-iris.org
 #
 #  This program is free software; you can redistribute it and/or
@@ -16,31 +16,30 @@
 #  along with this program; if not, write to the Free Software Foundation,
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import marshmallow
 from datetime import datetime
-
-from flask import request
 
 from app import db
 from app.datamgmt.case.case_comments import get_case_comment
 from app.iris_engine.module_handler.module_handler import call_modules_hook
 from app.iris_engine.utils.tracker import track_activity
-from app.schema.marshables import CommentSchema
-from app.util import response_error
-from app.util import response_success
-from app.business.case_comments import case_comments_update
 from app.business.errors import BusinessProcessingError
 
 
-def case_comment_update(comment_id, object_type, caseid):
-    try:
-        comment_schema = CommentSchema()
-        rq_t = request.get_json()
-        comment_text = rq_t.get('comment_text')
-        comment = case_comments_update(comment_text, comment_id, object_type, caseid)
-        return response_success("Comment edited", data=comment_schema.dump(comment))
-    except BusinessProcessingError as e:
-        return response_error(e.get_message(), data=e.get_data())
-    except marshmallow.exceptions.ValidationError as e:
-        return response_error(msg='Data error', data=e.normalized_messages())
+def case_comments_update(comment_text, comment_id, object_type, caseid):
+    comment = get_case_comment(comment_id, caseid=caseid)
+    if not comment:
+        raise BusinessProcessingError('Invalid comment ID')
 
+    comment.comment_text = comment_text
+    comment.comment_update_date = datetime.utcnow()
+
+    db.session.commit()
+
+    hook = object_type
+    if hook.endswith('s'):
+        hook = hook[:-1]
+
+    call_modules_hook(f'on_postload_{hook}_comment_update', data=comment, caseid=caseid)
+
+    track_activity(f"comment {comment.comment_id} on {object_type} edited", caseid=caseid)
+    return comment
