e6f6d483c5e97d8a4824f315b014cd7133c08ba5
whikernel <whitekernel@whitekernel.fr>
2024-08-09 10:32:43 +0200

    [FIX] Migration of IOC

--- source/app/alembic/versions/3715d4fac4de_add_link_between_case_and_ioc.py
+++ source/app/alembic/versions/3715d4fac4de_add_link_between_case_and_ioc.py
@@ -106,34 +106,39 @@ def upgrade():
                                                 "WHERE ioc_id = :ioc_id AND case_id = :case_id "),
                                            {"ioc_id": ioc.ioc_id, "case_id": ioc_link.case_id})
 
-                # Deleting the old ioc_asset_link links
-                conn.execute(text("DELETE FROM ioc_asset_link "
-                                  "JOIN case_assets ON ioc_asset_link.asset_id = case_assets.asset_id "
-                                  "WHERE ioc_id = :ioc_id AND case_id = :case_id "),
-                                {"ioc_id": ioc.ioc_id, "case_id": ioc_link.case_id})
+                asset_ids = [row.asset_id for row in asset_links]
 
-                # Inserting the new ioc_asset_link links
-                for asset_link in asset_links:
-                    conn.execute(text(
-                        "INSERT INTO ioc_asset_link(ioc_id, asset_id)"
-                        "VALUES (:ioc_id, :asset_id)"),
-                        {
-                            "ioc_id": new_ioc_id,
-                            "asset_id": asset_link.asset_id,
-                        })
+                if asset_ids:
+                    delete_query = text("""
+                        DELETE FROM ioc_asset_link 
+                        WHERE asset_id IN :asset_ids
+                    """)
+
+                    # Execute the DELETE query
+                    conn.execute(delete_query, {"asset_ids": tuple(asset_ids)})
+
+                    # Inserting the new ioc_asset_link links
+                    for asset_link in asset_ids:
+                        conn.execute(text(
+                            "INSERT INTO ioc_asset_link(ioc_id, asset_id)"
+                            "VALUES (:ioc_id, :asset_id)"),
+                            {
+                                "ioc_id": new_ioc_id,
+                                "asset_id": asset_link.asset_id,
+                            })
 
                 # duplicate case events ioc
-                case_event_links = conn.execute(text("SELECT * FROM case_event_ioc WHERE ioc_id = :ioc_id AND case_id = :case_id"),
+                case_event_links = conn.execute(text("SELECT * FROM case_events_ioc WHERE ioc_id = :ioc_id AND case_id = :case_id"),
                                                 {"ioc_id": ioc.ioc_id, "case_id": ioc_link.case_id})
 
                 # Deleting the old case_event_ioc links
-                conn.execute(text("DELETE FROM case_event_ioc WHERE ioc_id = :ioc_id AND case_id = :case_id"),
+                conn.execute(text("DELETE FROM case_events_ioc WHERE ioc_id = :ioc_id AND case_id = :case_id"),
                              {"ioc_id": ioc.ioc_id, "case_id": ioc_link.case_id})
 
                 # Inserting the new case_event_ioc links
                 for case_event_link in case_event_links:
                     conn.execute(text(
-                        "INSERT INTO case_event_ioc(case_event_id, ioc_id, case_id)"
+                        "INSERT INTO case_events_ioc(case_event_id, ioc_id, case_id)"
                         "VALUES (:case_event_id, :ioc_id, :case_id)"),
                         {
                             "case_event_id": case_event_link.case_event_id,
