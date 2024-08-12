ce3fc389699fda3305c411222dcfb1a1c9367bf5
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-08-12 15:09:59 +0200

    [IMP] Inverted conditions for improved readability

--- ui/src/pages/case.ioc.js
+++ ui/src/pages/case.ioc.js
@@ -147,43 +147,42 @@ function get_case_ioc() {
 
     get_request_data_api(`/api/v2/cases/${get_caseid()}/iocs`)
     .done((data, textStatus) => {
-        if (textStatus === 'success') {
-            if (data != null) {
-                jsdata = data;
-                Table.clear();
-                Table.rows.add(jsdata.iocs)
-
-                set_last_state(jsdata.state);
-                $('#ioc_table_wrapper').on('click', function(e){
-                    if($('.popover').length>1)
-                        $('.popover').popover('hide');
-                        $(e.target).popover('toggle');
-                    });
-
-                $('#ioc_table_wrapper').show();
-                Table.columns.adjust().draw();
-                load_menu_mod_options('ioc', Table, delete_ioc);
-                hide_loader();
-                Table.responsive.recalc();
-                $('[data-toggle="popover"]').popover();
-
-                $(document)
-                    .off('click', '.ioc_details_link')
-                    .on('click', '.ioc_details_link', function(event) {
-                    event.preventDefault();
-                    let ioc_id = $(this).data('ioc_id');
-
-                    edit_ioc(ioc_id);
-                });
-
-
-            } else {
-                Table.clear().draw();
-                swal("Oh no !", data.message, "error")
-            }
-        } else {
+        if (textStatus !== 'success') {
             Table.clear().draw()
+            return;
         }
+
+        if (data == null) {
+            Table.clear().draw();
+            swal("Oh no !", data.message, "error")
+            return;
+        }
+
+        Table.clear();
+        Table.rows.add(data.iocs)
+
+        set_last_state(data.state);
+        $('#ioc_table_wrapper').on('click', function(e){
+            if($('.popover').length>1)
+                $('.popover').popover('hide');
+                $(e.target).popover('toggle');
+            });
+
+        $('#ioc_table_wrapper').show();
+        Table.columns.adjust().draw();
+        load_menu_mod_options('ioc', Table, delete_ioc);
+        hide_loader();
+        Table.responsive.recalc();
+        $('[data-toggle="popover"]').popover();
+
+        $(document)
+            .off('click', '.ioc_details_link')
+            .on('click', '.ioc_details_link', function(event) {
+            event.preventDefault();
+            let ioc_id = $(this).data('ioc_id');
+
+            edit_ioc(ioc_id);
+        });
     })
 }
 
