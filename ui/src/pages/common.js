e8c5686227071f6fd5334ee92f7f6c382795e055
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-09-04 09:48:00 +0200

    [IMP] Inverted check

--- ui/src/pages/common.js
+++ ui/src/pages/common.js
@@ -507,50 +507,51 @@ function update_last_resfresh() {
 }
 
 function check_update(url) {
-    if (need_check) {
-        $.ajax({
-            url: url + case_param(),
-            type: "GET",
-            dataType: "json",
-            success: function(data) {
-                    if (last_state == null || last_state < data.data.object_state) {
-                        $('#last_resfresh').text("Updates available").addClass("text-warning");
-                        need_check = false;
-                    }
-                },
-            error: function (data) {
-                if (data.status == 404) {
-                    swal("Stop everything !",
-                    "The case you are working on was deleted",
-                    "error",
-                    {
-                        buttons: {
-                            again: {
-                                text: "Go to my default case",
-                                value: "default"
-                            }
-                        }
-                    }
-                    ).then((value) => {
-                        switch (value) {
-                            case "dash":
-                                location.reload();
-                                break;
-
-                            default:
-                                location.reload();
-                        }
-                    });
-                } else if (data.status == 403) {
-                    window.location.replace("/case" + case_param());
-                } else if (data.status == 400) {
-
-                } else {
-                    notify_error('Connection with server lost');
-                }
-            }
-        });
+    if (!need_check) {
+        return;
     }
+    $.ajax({
+        url: url + case_param(),
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+                if (last_state == null || last_state < data.data.object_state) {
+                    $('#last_resfresh').text("Updates available").addClass("text-warning");
+                    need_check = false;
+                }
+            },
+        error: function (data) {
+            if (data.status == 404) {
+                swal("Stop everything !",
+                "The case you are working on was deleted",
+                "error",
+                {
+                    buttons: {
+                        again: {
+                            text: "Go to my default case",
+                            value: "default"
+                        }
+                    }
+                }
+                ).then((value) => {
+                    switch (value) {
+                        case "dash":
+                            location.reload();
+                            break;
+
+                        default:
+                            location.reload();
+                    }
+                });
+            } else if (data.status == 403) {
+                window.location.replace("/case" + case_param());
+            } else if (data.status == 400) {
+
+            } else {
+                notify_error('Connection with server lost');
+            }
+        }
+    });
 }
 
 function set_last_state(state) {
