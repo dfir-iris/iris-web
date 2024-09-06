7c48c46198ed388f47de33af6ce66c7dec7a5e45
DevCyberT <177931443+DevCyberT@users.noreply.github.com>
2024-09-06 20:01:52 +0200

    [IMP] Wrap case page in svelte component

---
+++ ui/src/pages/case.svelte
@@ -0,0 +1,15 @@
+<script>
+    jQuery(document).ready(function(){
+        jQuery('#case_quick_status').change(function(){
+            post_request_api('/case/update-status', JSON.stringify({
+                'status_id': jQuery('#case_quick_status').val(),
+                'csrf_token': jQuery('#csrf_token').val()
+            }))
+            .done((data) => {
+                if (notify_auto_api(data)) {
+                    window.location.reload();
+                }
+            })
+        });
+    });
+</script>
