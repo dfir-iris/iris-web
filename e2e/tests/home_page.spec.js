0af6fb05f0f517b7c5c813ba548389b12a9c77ef
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-09-17 10:14:46 +0200

    [IMP] Added test Add customer modal can be opened

--- e2e/tests/home_page.spec.js
+++ e2e/tests/home_page.spec.js
@@ -11,3 +11,10 @@ test('successfully loads', async ({ page }) => {
     await expect(page.getByText('Invalid data type')).toBeVisible();
 });
 
+
+test('should be able to open "Add customer" modal', async ({ page }) => {
+    await page.goto('/manage/customers');
+    await page.getByRole('button', { name: 'Add customer' }).click();
+    await expect(page.getByRole('heading', { name: 'Add customer' })).toBeVisible()
+});
+
