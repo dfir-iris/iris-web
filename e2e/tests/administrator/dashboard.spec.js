1d4ef0bfe8aa303603ce229f32cf898445235948
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-09-18 11:09:59 +0200

    [IMP] Reorganized e2e tests

--- e2e/tests/administrator/dashboard.spec.js
+++ e2e/tests/administrator/dashboard.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.use({ storageState: 'playwright/.auth/administrator.json' })
 
-test('successfully loads', async ({ page }) => {
+test('create case with empty name should present error', async ({ page }) => {
     await page.goto('/dashboard');
     
     // FIXME: Should be a button instead of a link
@@ -12,11 +12,3 @@ test('successfully loads', async ({ page }) => {
     // FIXME: Locator should be: page.getByRole('alert', { name: 'Invalid data type' });
     await expect(page.getByText('Invalid data type')).toBeVisible();
 });
-
-// TODO move this to manage/customers.spec.js
-test('should be able to open "Add customer" modal', async ({ page }) => {
-    await page.goto('/manage/customers');
-    await page.getByRole('button', { name: 'Add customer' }).click();
-    await expect(page.getByRole('heading', { name: 'Add customer' })).toBeVisible()
-});
-
