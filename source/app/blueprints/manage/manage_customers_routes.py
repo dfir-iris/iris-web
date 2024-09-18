e27fa74f0160c91e3f6f5f2f0646534c45ef60c8
whikernel <whitekernel@whitekernel.fr>
2024-09-18 12:14:28 +0200

    [FIX] Removed customers checks on addition and modals

--- source/app/blueprints/manage/manage_customers_routes.py
+++ source/app/blueprints/manage/manage_customers_routes.py
@@ -340,7 +340,6 @@ def view_customers(client_id):
 
 @manage_customers_blueprint.route('/manage/customers/add/modal', methods=['GET'])
 @ac_requires(Permissions.customers_read, no_cid_required=True)
-@ac_requires_client_access()
 def add_customers_modal(caseid, url_redir):
     if url_redir:
         return redirect(url_for('manage_customers.manage_customers', cid=caseid))
@@ -351,7 +350,6 @@ def add_customers_modal(caseid, url_redir):
 
 @manage_customers_blueprint.route('/manage/customers/add', methods=['POST'])
 @ac_api_requires(Permissions.customers_write)
-@ac_api_requires_client_access()
 def add_customers():
     if not request.is_json:
         return response_error("Invalid request")
