09d1082b86bc65e0cd527fae9783f1f94e23ad18
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-09-27 11:16:48 +0200

    [IMP] Removed spurious white space

--- ui/src/pages/case.ioc.js
+++ ui/src/pages/case.ioc.js
@@ -100,7 +100,7 @@ function add_ioc() {
                     })
                 }
             } else {
-                case_id =  get_caseid()
+                case_id = get_caseid()
                 post_request_api(`/api/v2/cases/${case_id}/iocs` , JSON.stringify(data), true, function () {
                         $('#submit_new_ioc').text('Saving data..')
                             .attr("disabled", true)
