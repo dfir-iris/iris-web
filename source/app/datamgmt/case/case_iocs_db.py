2a43651c8fb2174720ee5f3e1ab3252b0e6b8f76
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-09-27 14:22:09 +0200

    [IMP] Removed unnecessary import

--- source/app/datamgmt/case/case_iocs_db.py
+++ source/app/datamgmt/case/case_iocs_db.py
@@ -16,8 +16,6 @@
 #  along with this program; if not, write to the Free Software Foundation,
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import logging
-
 from flask_login import current_user
 from sqlalchemy import and_
 from sqlalchemy import desc
