eb09e831dc87da5e6ce2d62823bf2b5a8c946e06
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-10-16 10:02:20 +0200

    [IMP] One import per line

--- source/app/blueprints/rest/v2/case/api_v2_ioc_routes.py
+++ source/app/blueprints/rest/v2/case/api_v2_ioc_routes.py
@@ -28,7 +28,8 @@ from app.blueprints.rest.endpoints import response_api_not_found
 from app.blueprints.rest.endpoints import response_api_success
 from app.business.errors import BusinessProcessingError
 from app.business.errors import ObjectNotFoundError
-from app.business.iocs import iocs_create, iocs_update
+from app.business.iocs import iocs_create
+from app.business.iocs import iocs_update
 from app.business.iocs import iocs_delete
 from app.business.iocs import iocs_get
 from app.datamgmt.case.case_iocs_db import get_filtered_iocs
