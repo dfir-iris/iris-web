193e833639e4f161d93c6ed28f02615d83c54073
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-10-16 14:32:24 +0200

    [IMP] Identation

--- ui/src/pages/common.js
+++ ui/src/pages/common.js
@@ -1355,13 +1355,13 @@ function hide_minimized_modal_box() {
 
 function hide_table_search_input(columns) {
     for (i=0; i<columns.length; i++) {
-      if (columns[i]) {
-        $('.filters th:eq(' + i + ')' ).show();
-      } else {
-        $('.filters th:eq(' + i + ')' ).hide();
-      }
+        if (columns[i]) {
+            $('.filters th:eq(' + i + ')' ).show();
+        } else {
+            $('.filters th:eq(' + i + ')' ).hide();
+        }
     }
-  }
+}
 
 function load_add_case() {
     // Dynamically load the modal
