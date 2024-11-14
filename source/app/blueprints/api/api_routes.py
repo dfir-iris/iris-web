d78400692ca1f38a81c505852124795b73216193
Maham Riaz Keyani <maham.keyani@idoubles.net>
2024-11-14 09:28:32 +0500

    Trigger response table created

--- source/app/blueprints/api/api_routes.py
+++ source/app/blueprints/api/api_routes.py
@@ -17,10 +17,12 @@
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from flask import Blueprint
+from flask_cors import CORS, cross_origin
 
 from app import app
 from app.util import ac_api_requires
 from app.util import response_success
+from flask import request, jsonify
 
 api_blueprint = Blueprint(
     'api',
@@ -28,13 +30,14 @@ api_blueprint = Blueprint(
     template_folder='templates'
 )
 
+CORS(api_blueprint, resources={r"/api/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # CONTENT ------------------------------------------------
 @api_blueprint.route('/api/ping', methods=['GET'])
 @ac_api_requires()
 def api_ping():
     return response_success("pong")
-
 
 @api_blueprint.route('/api/versions', methods=['GET'])
 @ac_api_requires()
@@ -46,3 +49,21 @@ def api_version():
     }
 
     return response_success(data=versions)
+
+@api_blueprint.route('/api/post-data', methods=['POST'])
+@cross_origin()
+def post_data():
+    # Get JSON data from the request body
+    data = request.get_json()
+
+    # If data is not provided, return an error
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    # Process the data (here, we simply return it as a response)
+    response = {
+        "message": "Data received successfully",
+        "received_data": data
+    }
+
+    return jsonify(response), 200
