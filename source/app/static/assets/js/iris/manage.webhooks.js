42c390356095534287ef80624207453a5ce432b4
Maham Riaz Keyani <maham.keyani@idoubles.net>
2024-11-19 14:09:48 +0500

    removed junk code

--- source/app/static/assets/js/iris/manage.webhooks.js
+++ source/app/static/assets/js/iris/manage.webhooks.js
@@ -38,41 +38,6 @@ $('#case_webhooks_table tbody').on('click', 'tr', function () {
   $('#webhookModal').modal('show');
 });
 
-// document.getElementById('copyModalContent').addEventListener('click', function() {
-//     // Select the elements containing the modal data
-//     const name = document.getElementById('webhookName').innerText;
-//     const description = document.getElementById('webhookDescription').innerText;
-
-//     // Format the content to be copied
-//     const modalContent = `Name: ${name}\nDescription: ${description}`;
-
-//     // Check if the browser supports navigator.clipboard
-//     if (navigator.clipboard && window.isSecureContext) {
-//         // Use Clipboard API for modern browsers
-//         navigator.clipboard.writeText(modalContent).then(() => {
-//             notify_success('Content copied to clipboard!');
-//         }).catch(err => {
-//             console.error('Failed to copy content: ', err);
-//             notify_success('Failed to copy content.');
-//         });
-//     } else {
-//         // Fallback method for older browsers
-//         const tempTextArea = document.createElement('textarea');
-//         tempTextArea.value = modalContent;
-//         document.body.appendChild(tempTextArea);
-//         tempTextArea.select();
-
-//         try {
-//             document.execCommand('copy');
-//             notify_success("content copied");
-//         } catch (err) {
-//             console.error('Failed to copy content: ', err);
-//             notify_error("failed to copy content");
-//         }
-
-//         document.body.removeChild(tempTextArea);
-//     }
-// });
 
 function add_webhook() {
   let url = "/manage/webhooks/add/modal" + case_param();
