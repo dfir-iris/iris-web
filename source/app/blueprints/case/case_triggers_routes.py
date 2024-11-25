68c8317281df68909b787e84fd4d83c2cab17e94
Maham Riaz Keyani <maham.keyani@idoubles.net>
2024-11-25 14:38:11 +0500

    Id-10 merged with id-5

--- source/app/blueprints/case/case_triggers_routes.py
+++ source/app/blueprints/case/case_triggers_routes.py
@@ -17,47 +17,31 @@
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 # IMPORTS ------------------------------------------------
-from datetime import datetime
-
-import marshmallow
 from flask import Blueprint
 from flask import redirect
 from flask import render_template
-from flask import request
 from flask import url_for
-from flask_login import current_user
 from flask_wtf import FlaskForm
-
-from app import db
-from app.blueprints.case.case_comments import case_comment_update
 from app.datamgmt.case.case_db import get_case
-from app.datamgmt.manage.manage_attribute_db import get_default_custom_attributes
-from app.datamgmt.states import get_tasks_state
 from app.datamgmt.states import update_tasks_state
-from app.forms import CaseTaskForm
-from app.iris_engine.module_handler.module_handler import call_modules_hook
-from app.iris_engine.utils.tracker import track_activity
 from app.models.authorization import CaseAccessLevel
-from app.models.authorization import User
-from app.schema.marshables import CommentSchema
 from app.util import ac_api_case_requires
 from app.util import ac_case_requires
-from app.util import response_error
-from app.util import response_success
+from source.app.datamgmt.manage.manage_case_response_db import get_case_responses_list
 
 case_triggers_blueprint = Blueprint('case_triggers',
-                                 __name__,
-                                 template_folder='templates')
-
+                                    __name__,
+                                    template_folder='templates')
 
 # CONTENT ------------------------------------------------
 @case_triggers_blueprint.route('/case/triggers', methods=['GET'])
-@ac_case_requires(CaseAccessLevel.read_only, CaseAccessLevel.full_access)
-def case_tasks(caseid, url_redir):
+#@ac_case_requires(CaseAccessLevel.read_only, CaseAccessLevel.full_access)
+def case_triggers(caseid, url_redir):
     if url_redir:
-        return redirect(url_for('case_tasks.case_tasks', cid=caseid, redirect=True))
+        return redirect(url_for('case_triggers.case_triggers', cid=caseid, redirect=True))
 
     form = FlaskForm()
     case = get_case(caseid)
+    triggers = get_case_responses_list()  # Fetch the triggers
 
-    return render_template("case_triggers.html", case=case, form=form)
+    return render_template("case_triggers.html", case=case, form=form, triggers=triggers)
