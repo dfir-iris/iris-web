a6c72cd2c0e360142a078fca137182ef5fbb2694
Amber Vale <me@ambervale.dev>
2024-11-30 11:31:32 -0500

    [ADD] Root v2 API blueprint + unauthz & biz error handling

---
+++ source/app/blueprints/rest/v2/__init__.py
@@ -0,0 +1,53 @@
+# Libraries
+from flask import Blueprint, has_request_context, request
+from flask_login import current_user
+
+# Common
+from source.app.blueprints.responses import response_error
+from source.app.blueprints.rest.endpoints import response_api_error
+
+# Child routes
+from source.app.blueprints.rest.v2.case import bp as v2_api_case
+
+# Authorization
+from source.app.blueprints.authorization.exceptions import UnauthorizedException
+from source.app.business.errors import BusinessProcessingError
+
+
+# MARK: Blueprint -------------------------------------------------------------
+
+
+# This blueprint will contain all the V2 API routes
+v2_api_bp = Blueprint('v2_api', __name__, url_prefix='/api/v2')
+
+# Register `/cases/` endpoints
+v2_api_bp.register_blueprint(v2_api_case)
+
+
+# MARK: Error Handling --------------------------------------------------------
+
+
+# Handlers --------------------------------------------------------------------
+
+def handle_unauthorized(exc: UnauthorizedException):
+    """Handles when `Unauthorized` is raised and returns a JSON error message."""
+    
+    error_data = {
+        "user_id": exc.args[0],
+        "endpoint": request.path if has_request_context() else None,
+        "resource": exc.args[1],
+        "action": exc.args[2],
+        "resource_id": exc.args[3]
+    }
+    
+    return response_error(f'Access denied while performing "{error_data["action"]}" on "{error_data["resource"]}".', data=error_data, status=403)
+
+def handle_business_processing_error(exc: BusinessProcessingError):
+    """Handles when `BusinessProcessingError` is raised and returns a JSON error message."""
+    
+    return response_api_error(exc.get_message(), exc.get_data())
+
+# Hook handlers onto their related exception ----------------------------------
+
+v2_api_bp.errorhandler(UnauthorizedException, handle_unauthorized)
+v2_api_bp.errorhandler(BusinessProcessingError, handle_business_processing_error)
