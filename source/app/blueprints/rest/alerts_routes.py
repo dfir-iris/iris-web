cb7681a2726336e55bf0959c9ece132d07d16675
BogatikovM <98744427+BogatikovM@users.noreply.github.com>
2024-12-04 20:06:20 +0400

    [647][IMP] alert_owner_id autofill

--- source/app/blueprints/rest/alerts_routes.py
+++ source/app/blueprints/rest/alerts_routes.py
@@ -433,6 +433,9 @@ def alerts_batch_update_route() -> Response:
             if not user_has_client_access(current_user.id, alert.alert_customer_id):
                 return response_error('User not entitled to update alerts for the client', status=403)
 
+            if getattr(alert, 'alert_owner_id') is None:
+                updates['alert_owner_id'] = current_user.id
+
             # Deserialize the JSON data into an Alert object
             alert_schema.load(updates, instance=alert, partial=True)
 
