bc62fb4e7f802d30466f654e8b687c113e559057
Gustav Alerby <g.alerby@gmail.com>
2024-12-11 15:07:17 +0100

    [ADD] Alembric script for adding the columns to the database

---
+++ source/app/alembic/versions/79ffcc7ef49e_add_external_ref_to_alerts.py
@@ -0,0 +1,36 @@
+"""Add external ref to alerts
+
+Revision ID: 79ffcc7ef49e
+Revises: 3715d4fac4de
+Create Date: 2024-12-11 09:38:22.382907
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+from app.alembic.alembic_utils import _table_has_column
+
+# revision identifiers, used by Alembic.
+revision = '79ffcc7ef49e'
+down_revision = '3715d4fac4de'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute('COMMIT')
+
+    if not _table_has_column('alerts', 'external_ref_id'):
+        op.add_column('alerts', sa.Column('external_ref_id', sa.Text, nullable=True))
+
+    if not _table_has_column('alerts', 'external_ref_link'):
+        op.add_column('alerts', sa.Column('external_ref_link', sa.Text, nullable=True))
+
+    if not _table_has_column('alerts', 'external_ref_source'):
+        op.add_column('alerts', sa.Column('external_ref_source', sa.JSON, nullable=True))
+
+    return
+
+
+def downgrade():
+    pass
