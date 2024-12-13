282c00d84ad415a5f3b2f125b9c90da071863f49
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-12-13 11:28:02 +0100

    Added an optional action based on the browser on which it is run

--- e2e/tests/administrator/dim/tasks.spec.js
+++ e2e/tests/administrator/dim/tasks.spec.js
@@ -9,7 +9,7 @@ test.beforeEach(async ({ page, rest }) => {
     await page.goto('/dim/tasks');
 });
 
-test('should be able to consult task info', async ({ page, rest }) => {
+test('should be able to consult task info', async ({ page, rest, browserName }) => {
     let response = await rest.get('/manage/modules/list');
     const modules = (await response.json()).data;
     const irisCheckModule = modules.find(module => module.module_human_name === 'IrisCheck');
@@ -26,9 +26,11 @@ test('should be able to consult task info', async ({ page, rest }) => {
     await page.locator('th:nth-child(5) > .form-group > .form-control').fill('on_postload_case_delete');
     // TODO should make the more interface testable to be able to use some page.getByRole
     await page.locator('td').getByRole('link').click();
-    // TODO this click should not be necessary. However, when run on the chromium browser, it seem the first click is ignored
-    //      => there is probably a bug in the code to chase
-    await page.locator('td').getByRole('link').click();
+    if (browserName === 'chromium') {
+        // TODO this click should not be necessary. However, when run on the chromium browser, it seem the first click is ignored
+        //      => there is probably a bug in the code to chase
+        await page.locator('td').getByRole('link').click();
+    }
 
     await expect(page.locator('#info_dim_task_modal_body')).toContainText('Module name: iris_check_module');
     await expect(page.locator('#info_dim_task_modal_body')).toContainText('Hook name: on_postload_case_delete');
