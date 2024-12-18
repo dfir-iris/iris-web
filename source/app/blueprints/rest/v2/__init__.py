f8c6f9c295f995eb92720feccadbf7634ce2792b
Amber Vale <me@ambervale.dev>
2024-12-18 18:00:32 -0500

    [ADD] Alerts v2 api to root v2 api BP

--- source/app/blueprints/rest/v2/__init__.py
+++ source/app/blueprints/rest/v2/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
 from app.blueprints.rest.v2.auth import auth_bp
+from app.blueprints.rest.v2.alerts import alerts_bp
 
 
 # Create root /api/v2 blueprint
@@ -9,3 +10,4 @@ rest_v2_bp = Blueprint("rest_v2", __name__, url_prefix="/api/v2")
 
 # Register child blueprints
 rest_v2_bp.register_blueprint(auth_bp)
+rest_v2_bp.register_blueprint(alerts_bp)
