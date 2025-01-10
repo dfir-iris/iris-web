d0cf9e7ba8f2025d52835f424f2bdc9e8c9d5c68
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-01-10 13:58:05 +0100

    [FIX] API V2 cases endpoints were disconnected. Tests failed

--- source/app/blueprints/rest/v2/__init__.py
+++ source/app/blueprints/rest/v2/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from app.blueprints.rest.v2.auth import auth_blueprint
 from app.blueprints.rest.v2.alerts import alerts_bp
 from app.blueprints.rest.v2.dashboard import dashboard_bp
+from app.blueprints.rest.v2.cases import api_v2_case_blueprint
 
 
 # Create root /api/v2 blueprint
@@ -10,6 +11,7 @@ rest_v2_bp = Blueprint("rest_v2", __name__, url_prefix="/api/v2")
 
 
 # Register child blueprints
+rest_v2_bp.register_blueprint(api_v2_case_blueprint)
 rest_v2_bp.register_blueprint(auth_blueprint)
 rest_v2_bp.register_blueprint(alerts_bp)
 rest_v2_bp.register_blueprint(dashboard_bp)
