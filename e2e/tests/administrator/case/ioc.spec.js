17603567158a71f4ce6b6e0b693843aecf90f4f3
whikernel <whitekernel@whitekernel.fr>
2025-01-14 14:19:18 +0100

    [FIX] IOC test issue due to UI upgrade

--- e2e/tests/administrator/case/ioc.spec.js
+++ e2e/tests/administrator/case/ioc.spec.js
@@ -12,12 +12,12 @@ test('should be able to update IOC', async ({ page }) => {
     await page.getByRole('button', { name: 'Add IOC' }).click();
     await page.getByRole('button', { name: 'None' }).click();
     await page.getByRole('listbox').getByRole('option', { name: 'AS', exact: true }).click();
-    await page.getByLabel('IOC Value').fill(iocValue);
+    await page.getByLabel('IOC Value *').fill(iocValue);
     await page.getByRole('button', { name: 'Save' }).click();
 
     await page.getByRole('link', { name: iocValue }).click();
     const newIocValue = `IOC value - ${crypto.randomUUID()}`;
-    await page.getByLabel('IOC Value').fill(newIocValue);
+    await page.getByLabel('IOC Value *').fill(newIocValue);
     await page.getByRole('button', { name: 'Update' }).click();
 
     await expect(page.getByRole('link', { name: newIocValue })).toBeVisible();
@@ -29,13 +29,13 @@ test('should not be able to create an IOC with the same type and value', async (
     await page.getByRole('button', { name: 'Add IOC' }).click();
     await page.getByRole('button', { name: 'None' }).click();
     await page.getByRole('listbox').getByRole('option', { name: 'AS', exact: true }).click();
-    await page.getByLabel('IOC Value').fill(iocValue);
+    await page.getByLabel('IOC Value *').fill(iocValue);
     await page.getByRole('button', { name: 'Save' }).click();
 
     await page.getByRole('button', { name: 'Add IOC' }).click();
     await page.getByRole('button', { name: 'None' }).click();
     await page.getByRole('listbox').getByRole('option', { name: 'AS', exact: true }).click();
-    await page.getByLabel('IOC Value').fill(iocValue);
+    await page.getByLabel('IOC Value *').fill(iocValue);
     await page.getByRole('button', { name: 'Save' }).click();
 
     await expect(page.getByText('IOC with same value and type already exists')).toBeVisible();
