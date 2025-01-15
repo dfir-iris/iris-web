dab1c49a1af4e8b25c127646d3c6f2385986cd29
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-01-15 08:35:32 +0100

    [FIX] Incorrect import

--- source/app/business/auth.py
+++ source/app/business/auth.py
@@ -1,3 +1,21 @@
+#  IRIS Source Code
+#  Copyright (C) 2024 - DFIR-IRIS
+#  contact@dfir-iris.org
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 from urllib.parse import urlsplit
 
 from flask import session, redirect, url_for, request
@@ -9,7 +27,7 @@ from app.datamgmt.manage.manage_users_db import get_active_user_by_login
 from app.iris_engine.access_control.ldap_handler import ldap_authenticate
 from app.iris_engine.access_control.utils import ac_get_effective_permissions_of_user
 from app.iris_engine.utils.tracker import track_activity
-from app.models import Cases
+from app.models.cases import Cases
 from app.schema.marshables import UserSchema
 
 log = app.logger
