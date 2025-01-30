60228250fbee13dafbd69a110786ee5fd587b995
whikernel <whitekernel@whitekernel.fr>
2025-01-30 14:18:57 +0100

    [FIX] Update of user case access from management

--- ui/src/pages/manage.cases.common.js
+++ ui/src/pages/manage.cases.common.js
@@ -182,37 +182,7 @@ function remove_protagonist(id) {
 }
 
 function remove_case_access_from_user(user_id, case_id, on_finish) {
-    swal({
-      title: "Are you sure?",
-      text: "This user might not be able access this case anymore",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, remove it!'
-    })
-    .then((willDelete) => {
-        if (willDelete) {
-            var data_sent = Object();
-            data_sent['case'] = case_id;
-            data_sent['csrf_token'] = $('#csrf_token').val();
-
-
-            post_request_api(`/manage/users/${user_id}/cases-access/delete`, JSON.stringify(data_sent))
-            .done((data) => {
-                if(notify_auto_api(data)) {
-
-                    if (on_finish !== undefined) {
-                        on_finish();
-                    }
-
-                }
-            }).always(() => {
-                window.swal.close();
-            });
-        }
-    });
+    return remove_cases_access_user(user_id, [case_id], on_finish);
 }
 
 var access_levels = [
