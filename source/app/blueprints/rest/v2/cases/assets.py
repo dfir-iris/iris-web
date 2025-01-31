50d1c0533cfa3b1abe490895f11e6bd8d948845e
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-01-31 13:16:27 +0100

    Renamed case_id into case_identifier

--- source/app/blueprints/rest/v2/cases/assets.py
+++ source/app/blueprints/rest/v2/cases/assets.py
@@ -39,28 +39,20 @@ from app.blueprints.access_controls import ac_api_return_access_denied
 
 case_assets_blueprint = Blueprint('case_assets',
                                   __name__,
-                                  url_prefix='/<int:case_id>/assets')
+                                  url_prefix='/<int:case_identifier>/assets')
 
 
 # TODO put this endpoint back once it adheres to the conventions (return value for paginated data: field assets should be field data, spurious field state, missing total, last_page, current_page and next_page)
 #@case_assets_blueprint.get('')
 @ac_api_requires()
-def case_list_assets(case_id):
-    """
-    Returns the list of assets from the case. 
+def case_list_assets(case_identifier):
 
-    Args:
-        case_id (int): The ID of the case this asset belongs too
-
-    Returns:
-        A JSON object containing the assets of the case, enhanced with assets seen on other cases.
-    """
     try:
 
-        if not ac_fast_check_current_user_has_case_access(case_id, [CaseAccessLevel.full_access]):
-            return ac_api_return_access_denied(caseid=case_id)
+        if not ac_fast_check_current_user_has_case_access(case_identifier, [CaseAccessLevel.full_access]):
+            return ac_api_return_access_denied(caseid=case_identifier)
 
-        assets = get_assets_case(case_identifier=case_id)
+        assets = get_assets_case(case_identifier=case_identifier)
 
         return response_api_success(data=assets)
 
@@ -72,21 +64,16 @@ def case_list_assets(case_id):
 
 @case_assets_blueprint.post('')
 @ac_api_requires()
-def add_asset(case_id):
-    """
-    Adds a new asset to a case
+def add_asset(case_identifier):
 
-    Args:
-        case_id (int): The ID of the case this asset is for
-    """
-    if not cases_exists(case_id):
+    if not cases_exists(case_identifier):
         return response_api_not_found()
-    if not ac_fast_check_current_user_has_case_access(case_id, [CaseAccessLevel.full_access]):
-        return ac_api_return_access_denied(caseid=case_id)
+    if not ac_fast_check_current_user_has_case_access(case_identifier, [CaseAccessLevel.full_access]):
+        return ac_api_return_access_denied(caseid=case_identifier)
 
     asset_schema = CaseAssetsSchema()
     try:
-        _, asset = assets_create(case_id, request.get_json())
+        _, asset = assets_create(case_identifier, request.get_json())
         return response_api_created(asset_schema.dump(asset))
     except BusinessProcessingError as e:
         return response_api_error(e.get_message())
@@ -94,22 +81,13 @@ def add_asset(case_id):
 
 @case_assets_blueprint.get('/<int:identifier>')
 @ac_api_requires()
-def get_asset(case_id, identifier):
-    """
-    Get an asset by case ID & asset ID
+def get_asset(case_identifier, identifier):
 
-    Args:
-        case_id (int): The ID of the case this asset belongs too
-        identifier (int): The asset ID to get
-    """
     asset_schema = CaseAssetsSchema()
 
     try:
         asset = assets_get(identifier)
-
-        # check asset & case ID match
-        if asset.case_id != case_id:
-            return response_api_not_found()
+        _check_asset_and_case_identifier_match(asset, case_identifier)
 
         # perform authz check
         if not ac_fast_check_current_user_has_case_access(asset.case_id, [CaseAccessLevel.read_only, CaseAccessLevel.full_access]):
@@ -124,9 +102,11 @@ def get_asset(case_id, identifier):
 
 @case_assets_blueprint.put('/<int:identifier>')
 @ac_api_requires()
-def update_asset(case_id, identifier):
+def update_asset(case_identifier, identifier):
     try:
         asset = assets_get(identifier)
+        _check_asset_and_case_identifier_match(asset, case_identifier)
+
         if not ac_fast_check_current_user_has_case_access(asset.case_id,[CaseAccessLevel.full_access]):
             return ac_api_return_access_denied(caseid=asset.case_id)
 
@@ -145,20 +125,11 @@ def update_asset(case_id, identifier):
 
 @case_assets_blueprint.delete('/<int:identifier>')
 @ac_api_requires()
-def delete_asset(case_id, identifier):
-    """
-    Handles deleting an asset by case ID & asset ID
+def delete_asset(case_identifier, identifier):
 
-    Args:
-        case_id (int): The ID of the case this asset belongs too
-        identifier (int): The asset ID to delete
-    """
     try:
         asset = assets_get(identifier)
-
-        # check asset & case ID match
-        if asset.case_id != case_id:
-            return response_api_not_found()
+        _check_asset_and_case_identifier_match(asset, case_identifier)
 
         # perform authz check
         if not ac_fast_check_current_user_has_case_access(asset.case_id, [CaseAccessLevel.full_access]):
@@ -170,3 +141,8 @@ def delete_asset(case_id, identifier):
         return response_api_not_found()
     except BusinessProcessingError as e:
         return response_api_error(e.get_message())
+
+
+def _check_asset_and_case_identifier_match(asset, case_identifier):
+    if asset.case_id != case_identifier:
+        raise ObjectNotFoundError
