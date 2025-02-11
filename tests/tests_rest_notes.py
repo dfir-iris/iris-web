9e3c81c3ca1fe1a38db39334ec95eb2134df1af6
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-02-11 15:15:46 +0100

    POST /api/v2/cases/{case_identifier}/notes returns 400 when directory with directory_id does not exist

--- tests/tests_rest_notes.py
+++ tests/tests_rest_notes.py
@@ -78,3 +78,9 @@ class TestsRestNotes(TestCase):
         body = {'directory_id': directory_identifier}
         response = self._subject.create(f'/api/v2/cases/{_IDENTIFIER_FOR_NONEXISTENT_OBJECT}/notes', body)
         self.assertEqual(404, response.status_code)
+
+    def test_create_note_with_missing_directory_should_return_404(self):
+        case_identifier = self._subject.create_dummy_case()
+        body = {'directory_id': _IDENTIFIER_FOR_NONEXISTENT_OBJECT}
+        response = self._subject.create(f'/api/v2/cases/{case_identifier}/notes', body)
+        self.assertEqual(400, response.status_code)
