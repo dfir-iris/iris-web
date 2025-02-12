d3d59a493d35ff8fb726bdd57badc0137ec8e6a1
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-02-12 13:48:13 +0100

    Added GET /api/v2/cases/{case_identifier}/notes/{identifier} to get a note

--- source/app/blueprints/rest/v2/cases/notes.py
+++ source/app/blueprints/rest/v2/cases/notes.py
@@ -21,15 +21,19 @@ from flask import request
 
 from app.blueprints.access_controls import ac_api_requires
 from app.blueprints.rest.endpoints import response_api_created
+from app.blueprints.rest.endpoints import response_api_success
 from app.blueprints.rest.endpoints import response_api_error
 from app.blueprints.rest.endpoints import response_api_not_found
 from app.blueprints.access_controls import ac_api_return_access_denied
 from app.schema.marshables import CaseNoteSchema
 from app.models.authorization import CaseAccessLevel
+from app.models.models import Notes
 from app.iris_engine.access_control.utils import ac_fast_check_current_user_has_case_access
 from app.business.notes import notes_create
+from app.business.notes import notes_get
 from app.business.cases import cases_exists
 from app.business.errors import BusinessProcessingError
+from app.business.errors import ObjectNotFoundError
 
 
 case_notes_blueprint = Blueprint('case_notes',
@@ -54,3 +58,28 @@ def create_note(case_identifier):
 
     except BusinessProcessingError as e:
         return response_api_error(e.get_message(), data=e.get_data())
+
+
+@case_notes_blueprint.get('/<int:identifier>')
+@ac_api_requires()
+def get_note(case_identifier, identifier):
+
+    note_schema = CaseNoteSchema()
+
+    try:
+        note = notes_get(identifier)
+        _check_note_and_case_identifier_match(note, case_identifier)
+
+        if not ac_fast_check_current_user_has_case_access(note.note_case_id, [CaseAccessLevel.read_only, CaseAccessLevel.full_access]):
+            return ac_api_return_access_denied(caseid=note.note_case_id)
+
+        return response_api_success(note_schema.dump(note))
+    except ObjectNotFoundError:
+        return response_api_not_found()
+    except BusinessProcessingError as e:
+        return response_api_error(e.get_message())
+
+
+def _check_note_and_case_identifier_match(note: Notes, case_identifier):
+    if note.note_case_id != case_identifier:
+        raise ObjectNotFoundError
