07bd35fe464b683538da7bdf7fad1c0139f06c25
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-02-28 08:50:31 +0100

    Added tests to check that udpate note return 404 when identifiers do not correspond to existing objects

--- tests/tests_rest_notes.py
+++ tests/tests_rest_notes.py
@@ -175,3 +175,19 @@ class TestsRestNotes(TestCase):
         identifier = response['note_id']
         response = self._subject.update(f'/api/v2/cases/{case_identifier}/notes/{identifier}', {'directory_id': _IDENTIFIER_FOR_NONEXISTENT_OBJECT})
         self.assertEqual(400, response.status_code)
+
+    def test_update_note_should_return_404_when_case_identifier_does_not_exist(self):
+        case_identifier = self._subject.create_dummy_case()
+        response = self._subject.create('/case/notes/directories/add',
+                                        {'name': 'directory_name'}, query_parameters={'cid': case_identifier}).json()
+        directory_identifier = response['data']['id']
+        body = {'directory_id': directory_identifier}
+        response = self._subject.create(f'/api/v2/cases/{case_identifier}/notes', body).json()
+        identifier = response['note_id']
+        response = self._subject.update(f'/api/v2/cases/{_IDENTIFIER_FOR_NONEXISTENT_OBJECT}/notes/{identifier}', {})
+        self.assertEqual(404, response.status_code)
+
+    def test_update_note_should_return_404_when_identifier_does_not_exist(self):
+        case_identifier = self._subject.create_dummy_case()
+        response = self._subject.update(f'/api/v2/cases/{case_identifier}/notes/{_IDENTIFIER_FOR_NONEXISTENT_OBJECT}', {})
+        self.assertEqual(404, response.status_code)
