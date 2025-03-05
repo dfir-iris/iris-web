7891a03d7ec4784df8c0463423b025e581a06e13
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-03-05 14:51:37 +0100

    Removed doubled slash

--- tests/tests_auth.py
+++ tests/tests_auth.py
@@ -62,7 +62,7 @@ class TestsAuth(TestCase):
         user_name = f'user{uuid4()}'
         password = 'aA.1234567890'
         self._subject.create_user(user_name, password)
-        url = parse.urljoin(API_URL, '/api/v2//auth/login')
+        url = parse.urljoin(API_URL, '/api/v2/auth/login')
         response = requests.post(url, json={'username': user_name, 'password': password})
         self.assertIn('Set-Cookie', response.headers)
 
