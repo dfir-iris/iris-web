256bbaaa3eb37a7f3412c11f70853ab8a866f7bb
Ch1mCh4 <info@jleppert.de>
2025-12-15 10:35:08 -0500

    bump vversion

--- source/app/configuration.py
+++ source/app/configuration.py
@@ -230,7 +230,7 @@ class CeleryConfig:
 
 class Config:
     # Handled by bumpversion
-    IRIS_VERSION = "v2.5.0-beta.1" # DO NOT EDIT THIS LINE MANUALLY
+    IRIS_VERSION = "v2.5.0-procyde.1" # DO NOT EDIT THIS LINE MANUALLY
 
     if os.environ.get('IRIS_DEMO_VERSION') is not None and os.environ.get('IRIS_DEMO_VERSION') != 'None':
         IRIS_VERSION = os.environ.get('IRIS_DEMO_VERSION')
