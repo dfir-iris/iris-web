1cbdd21d443cbd2c45b23a62e2314e1e51b67c77
0xr2po <117746006+0xr2po@users.noreply.github.com>
2026-01-12 13:10:53 +0100

    Bump IRIS version to v2.5.0-beta.1-dev-pr2

--- source/app/configuration.py
+++ source/app/configuration.py
@@ -230,7 +230,7 @@ class CeleryConfig:
 
 class Config:
     # Handled by bumpversion
-    IRIS_VERSION = "v2.5.0-beta.1-dev-pr1" # DO NOT EDIT THIS LINE MANUALLY
+    IRIS_VERSION = "v2.5.0-beta.1-dev-pr2" # DO NOT EDIT THIS LINE MANUALLY
 
     if os.environ.get('IRIS_DEMO_VERSION') is not None and os.environ.get('IRIS_DEMO_VERSION') != 'None':
         IRIS_VERSION = os.environ.get('IRIS_DEMO_VERSION')
