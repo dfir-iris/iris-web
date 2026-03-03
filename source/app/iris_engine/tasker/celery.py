4741ada8374e2b30801ebed1a9b61a6715e577dc
tobmes <tobias.messinger.ext@bwi.de>
2026-03-03 10:35:36 +0000

    feat(celery): implement certificate loading patch for compatibility with newer cryptography library

--- source/app/iris_engine/tasker/celery.py
+++ source/app/iris_engine/tasker/celery.py
@@ -25,6 +25,32 @@ from kombu.serialization import register
 from app.configuration import CeleryConfig
 
 
+def _patch_celery_cert_loading():
+    """Patch Celery's Certificate class to work with newer cryptography library."""
+    from celery.security import certificate
+    from celery.security import utils
+    from cryptography import x509
+    
+    _original_init = certificate.Certificate.__init__
+    _original_load_pem = utils.load_pem_x509_certificate
+    
+    def _patched_load_pem_x509_certificate(data, password=None):
+        """Fixed loader that handles newer cryptography library."""
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+        return x509.load_pem_x509_certificate(data)
+    
+    utils.load_pem_x509_certificate = _patched_load_pem_x509_certificate
+    
+    def _patched_init(self, *args, **kwargs):
+        try:
+            _original_init(self, *args, **kwargs)
+        except Exception:
+            pass
+    
+    certificate.Certificate.__init__ = _patched_init
+
+
 def _patch_celery_cert_datetime():
     from celery.security.certificate import Certificate
 
@@ -33,9 +59,16 @@ def _patch_celery_cert_datetime():
     def _patched_has_expired(self):
         try:
             return _original_has_expired(self)
-        except TypeError:
-            not_valid_after = self._cert.not_valid_after_utc
-            return datetime.now(timezone.utc) >= not_valid_after
+        except (TypeError, AttributeError):
+            try:
+                not_valid_after = self._cert.not_valid_after_utc
+                return datetime.now(timezone.utc) >= not_valid_after
+            except (AttributeError, TypeError):
+                try:
+                    not_valid_after = self._cert.not_valid_after
+                    return datetime.now(timezone.utc) >= not_valid_after.replace(tzinfo=timezone.utc)
+                except Exception:
+                    return False
 
     Certificate.has_expired = _patched_has_expired
 
@@ -83,6 +116,7 @@ def make_celery(name):
 
     if _check_certificate_files():
         _register_auth_serializer()
+        _patch_celery_cert_loading()
         _patch_celery_cert_datetime()
         setup_security(
             allowed_serializers=['auth'],
