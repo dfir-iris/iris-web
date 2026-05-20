9073d7bc0390944d9a6ca4d33cea597861431fb0
tobmes <tobias.messinger.ext@bwi.de>
2026-05-20 09:54:30 +0000

    chore(version): revert IRIS_VERSION to v2.5.0-beta.1-dev-pr3

--- source/app/configuration.py
+++ source/app/configuration.py
@@ -369,7 +369,7 @@ for _setting, _parse in _celery_settings:
 
 class Config:
     # Handled by bumpversion
-    IRIS_VERSION = "v2.5.0-beta.1-dev-pr8" # DO NOT EDIT THIS LINE MANUALLY
+    IRIS_VERSION = "v2.5.0-beta.1-dev-pr3" # DO NOT EDIT THIS LINE MANUALLY
 
     if os.environ.get('IRIS_DEMO_VERSION') is not None and os.environ.get('IRIS_DEMO_VERSION') != 'None':
         IRIS_VERSION = os.environ.get('IRIS_DEMO_VERSION')
