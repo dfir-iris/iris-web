a66ec94d6ee66efdeaf10e74f48c42014feb761e
whitekernel <whitekernel@dfir-iris.org>
2026-06-24 10:52:04 +0200

    [ADD] v2 endpoints for managing modules and their hooks

--- source/app/blueprints/rest/v2/manage.py
+++ source/app/blueprints/rest/v2/manage.py
@@ -22,6 +22,7 @@ from app.blueprints.rest.v2.manage_routes.groups import groups_blueprint
 from app.blueprints.rest.v2.manage_routes.users import users_blueprint
 from app.blueprints.rest.v2.manage_routes.customers import customers_blueprint
 from app.blueprints.rest.v2.manage_routes.server import server_blueprint
+from app.blueprints.rest.v2.manage_routes.modules import modules_blueprint
 
 manage_v2_blueprint = Blueprint("manage", __name__, url_prefix="/manage")
 
@@ -29,3 +30,4 @@ manage_v2_blueprint.register_blueprint(groups_blueprint)
 manage_v2_blueprint.register_blueprint(users_blueprint)
 manage_v2_blueprint.register_blueprint(customers_blueprint)
 manage_v2_blueprint.register_blueprint(server_blueprint)
+manage_v2_blueprint.register_blueprint(modules_blueprint)
