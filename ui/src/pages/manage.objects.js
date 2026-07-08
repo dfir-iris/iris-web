f468645d207494b606bd1ac8eb568b6dcd07dc6e
tobmes <tobias@messinger.it>
2026-07-08 12:07:45 +0000

    feat(asset-type): enhance asset icon management with existing icon selection

--- ui/src/pages/manage.objects.js
+++ ui/src/pages/manage.objects.js
@@ -89,6 +89,62 @@ function refresh_asset_table() {
   notify_success("Refreshed");
 }
 
+/* Populate the "choose existing icon" dropdowns in the asset type modal.
+   current_nc / current_c are the asset type's currently assigned icon filenames (if any). */
+function init_asset_type_icon_selects(current_nc, current_c) {
+    $.getJSON(`/manage/asset-type/icons${case_param()}`, function (icons) {
+        function fmtIcon(opt) {
+            if (!opt.id) {
+                return opt.text;
+            }
+            return $('<span><img src="' + opt.path + '" style="width:1.5em;height:1.5em;margin-right:0.5em">' + opt.text + '</span>');
+        }
+
+        function buildOptions(selected) {
+            var data = [{ id: '', text: '-- Upload new --' }];
+            icons.forEach(function (i) {
+                data.push({ id: i.name, text: i.name, path: i.path, selected: (i.name === selected) });
+            });
+            return data;
+        }
+
+        $('#existing_icon_not_compromised_select').select2({
+            data: buildOptions(current_nc),
+            templateResult: fmtIcon,
+            templateSelection: fmtIcon,
+            width: '100%'
+        }).on('change', function () {
+            $('#existing_icon_not_compromised').val($(this).val());
+            if ($(this).val()) {
+                $('#asset_icon_not_compromised').val('');
+            }
+        });
+
+        $('#existing_icon_compromised_select').select2({
+            data: buildOptions(current_c),
+            templateResult: fmtIcon,
+            templateSelection: fmtIcon,
+            width: '100%'
+        }).on('change', function () {
+            $('#existing_icon_compromised').val($(this).val());
+            if ($(this).val()) {
+                $('#asset_icon_compromised').val('');
+            }
+        });
+
+        $('#asset_icon_not_compromised').on('change', function () {
+            if (this.value) {
+                $('#existing_icon_not_compromised_select').val('').trigger('change');
+            }
+        });
+        $('#asset_icon_compromised').on('change', function () {
+            if (this.value) {
+                $('#existing_icon_compromised_select').val('').trigger('change');
+            }
+        });
+    });
+}
+
 
 /* Fetch the details of an asset and allow modification */
 function assettype_detail(asset_id) {
